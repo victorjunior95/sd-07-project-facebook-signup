923ecf0ce900b7d4fb31d912ad1d89f8c270d4a7
mayconfelipes <mayconfelipe.dev@gmail.com>
2020-09-24 18:54:24 -0300

    Começando Requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const login = document.getElementById('button-login');
 const user = document.getElementById('user-email-phone');
 
+const name = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const phoneEmail = document.getElementById('phone_email');
+const password = document.getElementById('password');
+const birthdate = document.getElementById('birthdate');
+const cadastro = document.getElementById('facebook-register');
+
+
 login.addEventListener('click', function () {
   alert(user.value);
+});
+
+cadastro.addEventListener('click', function () {
+  if ((name.value === '') || (lastname.value === '') || (phoneEmail.value === '') || (password.value === '') || (birthdate.value === '')) {
+    alert('Campos inválidos');
+  }
 });

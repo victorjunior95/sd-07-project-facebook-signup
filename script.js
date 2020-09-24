15b646c83372b85bd26ea8b34964591100ab3925
Julio Kauer <juliokauer@gmail.com>
2020-09-24 18:17:31 -0300

    Ate requisito 7

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const loginButton = document.querySelector("#button-login");
+const loginInput = document.querySelector("#login");
 
+loginButton.addEventListener('click', function () {
+  alert(`${loginInput.value}`);
+}
+) 

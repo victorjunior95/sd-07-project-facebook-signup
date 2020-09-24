8d5e2db2c4da244dcbe646c66163fad02c500b3d
afonsofn <afonsoafn@gmail.com>
2020-09-24 18:45:12 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let emailTelefone = document.querySelector("#user-email-phone")
+let loginButton = document.querySelector("#button-login")
+loginButton.addEventListener("click", function() {
+    alert(emailTelefone.value)
+})

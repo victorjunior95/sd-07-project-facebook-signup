e0ff00c2b5b855ad07cfa3c0540138489692fa3c
Breno Alves <brenoalv3ss@gmail.com>
2020-09-24 18:52:29 -0300

    Correção

---
+++ script.js
@@ -0,0 +1,6 @@
+const email = document.getElementById('user-email-phone');
+const button = document.getElementById('button-login');
+
+button.addEventListener('click', function () {
+  alert(email.value);
+});

8c6820548de3e2379aac121ae21822452bd9d98d
alvaro <alvarovascon2@gmail.com>
2020-09-24 19:40:33 -0300

    alert?

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const loginButton = document.getElementById('button-login');
+const formInputs = document.querySelectorAll('.new-acount input');
+const registerButton = document.getElementById('facebook-register');
 
 loginButton.addEventListener('click', function () {
   const emailOrTel = document.getElementById('user-email-phone');
   alert(emailOrTel.value);
+});
+
+registerButton.addEventListener('click', function () {
+  for (let index = 0; index < formInputs.length; index += 1) {
+    if (formInputs[index].value === '') {
+      alert("Campos inválidos");
+      return;
+    }
+  }
 });

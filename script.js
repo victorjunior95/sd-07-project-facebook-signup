caab02daed758f1c62f4f5641fc2dfbef67a466f
Arthur Nunes <coletivosuburbano@gmail.com>
2020-09-24 16:41:03 -0300

    refatorando codeclimate

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-buttonEntrar = document.querySelector("#button-login");
-emailOrTel = document.querySelector("#user-email-phone");
 
-buttonEntrar.addEventListener("click", function () {
-  alert(emailOrTel.value);
-});

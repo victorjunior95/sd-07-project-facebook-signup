f8e83f9faa6610501031d9c057179397073951e0
Lucas Andrade <lafdesouza2002@outlook.com>
2020-09-24 19:11:45 -0300

    Adicionando requisito 18

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+const inputs = document.querySelectorAll('.right-content input')
+
 document.querySelector('#button-login').addEventListener('click', () => {
   alert(document.querySelector('#user-email-phone').value);
 });
 
+document.querySelector("#facebook-register").addEventListener('click', (event) => {
+  for (let i = 0; i < inputs.length; i += 1) {
+    if (inputs[i].value == '') {
+      event.preventDefault()
+      document.querySelector('.handle-error').innerHTML = 'Campo inválido'
+    }
+  }
+})

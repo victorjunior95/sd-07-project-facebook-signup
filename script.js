36b46828bd75156ec16778aca90dbb448cef93f2
Wendrick <wendrick.b.c1@gmail.com>
2020-09-24 18:31:23 -0300

    Muitos requisitos depois

---
+++ script.js
@@ -0,0 +1,6 @@
+let buttonEnter = document.querySelector("#button-login");
+buttonEnter.addEventListener("click", function() {
+    let emailPhone = document.querySelector("#user-email-phone");
+    let password = document.querySelector("#user-passowrd");
+    alert(emailPhone.value);
+});

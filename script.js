915d39c986b118b2ef743846d6ecd886ab57e942
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-09-24 16:57:44 -0300

    Requisito 7 - Adicionando o terceiro subcontainer

---
+++ script.js
@@ -0,0 +1,6 @@
+const loginButton = document.getElementById("button-login");
+
+loginButton.addEventListener("click", function() {
+  const emailInput = document.getElementById("user-email-phone").value;
+  alert(emailInput);
+});

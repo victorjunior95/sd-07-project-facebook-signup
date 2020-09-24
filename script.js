fcd4a6c5b1e87dbcc37c14fcaef508659fe0305d
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-24 17:29:27 -0300

    Testando mais requisitos

--- script.js
+++ script.js
@@ -8,4 +8,14 @@ buttonLogin.addEventListener('click', function () {
 const buttonRegister = document.querySelector('#facebook-register');
 buttonRegister.addEventListener('click', function () {
   let inputs = document.querySelectorAll('.main-form input');
+
+  for (let i = 0; i < inputs.length; i += 1) {
+    if (inputs[i].value === '') {
+      let p = document.createElement('p');
+      const mainForm = document.querySelector('.main-form');
+      mainForm.appendChild(p);
+      p.innerHTML = 'Campos inválidos';
+      break;
+    }
+  }
 });

9a3b761d3743e00c56ea199e539d05e17ad526ce
Wendrick <wendrick.b.c1@gmail.com>
2020-09-24 19:05:09 -0300

    Corrigindo o CC part 2 o retorno

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const buttonEnter = document.querySelector('#button-login');
-buttonEnter.addEventListener( 'click', function() {
-  const emailPhone = document.querySelector('#user-email-phone');
-  const password = document.querySelector('#user-passowrd');
+buttonEnter.addEventListener('click', function () {
+  const emailPhone = document.querySelector('#user-email-phone'); 
   alert(emailPhone.value);
 });

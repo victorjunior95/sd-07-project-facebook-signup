6fef08125311ae77b716bf07258f997e043f28a8
Gustavo Bregalda <gus.bregalda@gmail.com>
2020-09-24 16:15:10 -0300

    Requisitos 5, 6 e 7

---
+++ script.js
@@ -0,0 +1,7 @@
+const senha = document.querySelector("#user-password");
+const user = document.querySelector("#user-email-phone");
+const button = document.querySelector("#button-login");
+
+button.addEventListener(`click`, function() {
+  alert("Email ou telefone");
+})

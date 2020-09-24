f4e28bd5bf3af7a14989177632b217e3881b8750
Rodrigo <rd64103@gmail.com>
2020-09-24 16:21:51 -0300

    teste do botao

---
+++ script.js
@@ -0,0 +1,7 @@
+const login = document.getElementById('user-email-phone-label').value
+const senha = document.getElementById('user-password-label').value
+const button = document.getElementById('button-login')
+
+button.addEventListener('click', function(){
+    console.log("sda");
+})

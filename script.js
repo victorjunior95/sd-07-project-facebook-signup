1feff1305b1e20c76bf6f4dd20d62985c8f5dd72
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-09-24 18:09:17 -0300

    Requisitos 17 e 18 - 24/09/2020

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const loginInput = document.querySelector("#user-email-phone");
 buttonForm.addEventListener("click", function () {
   alert(loginInput.value);
 })
+
+const rightContentInputs = document.querySelector("#sign-up-container").querySelectorAll("input");
+
+document.querySelector("#facebook-register").addEventListener("click", function () {
+  rightContentInputs.forEach(item => {
+    item.required = "required";
+
+    if(item.value == "") {
+      item.value = "Campos inválidos";
+    }
+  }) 
+})

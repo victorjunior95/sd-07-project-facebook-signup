17e491068feef8071eebaba03c7c8d7b337f24e3
Muniz <vinicius.muniz.marin@gmail.com>
2020-09-24 19:14:18 -0300

    requisitos 18 e 19

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const btnLogin = document.querySelector('#button-login');
 const user = document.querySelector('#user-email-phone');
 btnLogin.addEventListener('click', function () {
-    alert(user.value);
+  alert(user.value);
 });
 
-const radioCustomGender = document.getElementsByName("gender")[2];
+const radioCustomGender = document.getElementsByName('gender')[2];
 radioCustomGender.addEventListener('click', function () {
-    let newAccForm = document.querySelector('.new-account');
-    let genderCustom = document.createElement('input');
-    genderCustom.type = "text";
-    genderCustom.name = "gender-custom";
-    genderCustom.placeholder = "Gênero (opcional)";
-    newAccForm.appendChild(genderCustom);
-    newAccForm.insertBefore(genderCustom, newAccForm.children[19]);
-})
+  let newAccForm = document.querySelector('.new-account');
+  let genderCustom = document.createElement('input');
+  genderCustom.type = "text";
+  genderCustom.name = "gender-custom";
+  genderCustom.placeholder = "Gênero (opcional)";
+  newAccForm.appendChild(genderCustom);
+  newAccForm.insertBefore(genderCustom,  newAccForm.children[19]);
+});
 
 
 const inputFirstName = document.getElementsByName('firstname')[0];
@@ -25,14 +25,11 @@ const inputGender = document.getElementsByName('gender');
 const btnSignUp = document.querySelector('#facebook-register');
 
 
-btnSignUp.addEventListener('click', function(){
-    console.log('teste')
-    let genderChecked = false;
-
-    if (inputGender[0].checked || inputGender[1].checked || inputGender[2].checked) {
+btnSignUp.addEventListener('click', function() {
+  let genderChecked = false;
+  if (inputGender[0].checked || inputGender[1].checked || inputGender[2].checked) {
         genderChecked = true;
-    }
-
+  }
     if (inputFirstName.value === null || inputLastName.value === null || inputPhoneEmail.value === null || inputPassword.value === null || inputBirthdate.value === null || genderChecked === false) {
         alert('Campos inválidos');
     }

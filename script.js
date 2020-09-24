437e1193a1a73484f4cacec808026258bdd7ae72
barrrosomarcio <barrrosomarcio@gmail.com>
2020-09-24 20:24:12 -0300

    correcoes CC

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const alertText = document.querySelector('#user-email-phone');
 const alertButton = document.querySelector('#button-login');
 
 function sendAlert() {
-    alert(alertText);
-  }
+  alert(alertText);
+}
 
 alertButton.addEventListener('click', sendAlert());

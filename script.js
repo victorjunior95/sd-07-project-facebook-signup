8255e8f6e6ee65cadff94ca7264da773101a3adc
Vanessa Oliveira <vanessanaara@gmail.com>
2020-09-24 16:54:31 -0300

    Correçao dos requisitos 2, 3, 5 e 6

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//teste
+const button = document.querySelector('#button-login');
+const inputEmail = document.querySelector('#user-email-phone');
+
+button.addEventListener('click', function alertEmail() {
+  alert(inputEmail.value);
+});

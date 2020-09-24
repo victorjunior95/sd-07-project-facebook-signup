ba32d1b1f204abc74cb11cd53c53e7636363ad06
Rodolfo <rodolfojr94@gmail.com>
2020-09-24 16:58:17 -0300

    Problemas no botão enviar

---
+++ script.js
@@ -0,0 +1,5 @@
+let submitBt = document.querySelector("#button-login");
+let userIp = document.querySelector("#user-email-phone").value;
+
+//Não funciona!
+submitBt.addEventListener('click' ,  console.log( 'aqui' ));

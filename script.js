c3c5ab2b74c461b662196b43cadfc8ad0f1731f9
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-09-24 18:27:29 -0300

    atualizei

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 /* validate form empty */
+let firstname = document.forms['form-facebook']['firstname'].value;
+let lastname = document.forms['form-facebook']['lastname'].value;
+let phone_email = document.forms['form-facebook']['phone_email'].value;
+let password = document.forms['form-facebook']['password'].value;
+let birthdate = document.forms['form-facebook']['birthdate'].value;
+let gender = document.forms['form-facebook']['gender'].value;
 
+const btnValidate = document.getElementById('facebook-register');
+btnValidate.addEventListener('click', function(){
+  if (firstname == '' && lastname == '') {
 
+  }
+});
 /* validate form empty end */

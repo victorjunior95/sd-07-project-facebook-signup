c45ea6a38ac31358cabf6aa4155016f80daddfde
menezesandre <dehhmenezes87@gmail.com>
2020-09-24 16:26:26 -0300

    Requisitos do 1 ao 7 prontos

---
+++ script.js
@@ -0,0 +1,5 @@
+const btnLogin = document.querySelector('#button-login');
+const user = document.querySelector('#user-email-phone');
+btnLogin.addEventListener('click', function() {
+	alert(user.value);
+})

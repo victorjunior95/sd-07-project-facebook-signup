3017ac98a4b91e3729dc48c63c09bdcb1c570008
TIAGO <tiagoesdrasvieira@gmail.com>
2020-09-24 17:28:27 -0300

    Fix codeclimate

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const loginBtn = document.querySelector('#button-login');
-loginBtn.addEventListener('click', function(event) {
+loginBtn.addEventListener('click', function (event) {
   event.preventDefault();
-  let emailInput = document.querySelector('#user-email-phone').value;
-  let passwordInput = document.querySelector('#user-password').value;
-  alert(emailInput + ' ' + passwordInput);
+  const emailInput = document.querySelector('#user-email-phone').value;
+  const passwordInput = document.querySelector('#user-password').value;
+  alert(`${emailInput} ${passwordInput}`);
 });

ed69629b4c0076a7c1b80623a161a73fcc0da528
Anderson Santana <anderslash@gmail.com>
2020-09-24 18:32:10 -0300

    Correçao do js para o codeclimate aceitar

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.getElementById('button-login');
-button.addEventListener('click', function(event){
-    let user = document.getElementById('user-email-phone').value;
-    alert(user);
+button.addEventListener('click', function () {
+  const user = document.getElementById('user-email-phone').value;
+  alert(user);
 });

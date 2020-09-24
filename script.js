4f0f66b2c1b692caaed11075716a6855baa9a18c
barrrosomarcio <barrrosomarcio@gmail.com>
2020-09-24 20:16:48 -0300

    correções codeclimate

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function alerta(){
-    alert("Email ou telefone");
+let alertText = document.querySelector("#user-email-phone");
+let alertButton = document.querySelector("#button-login");
+
+alertButton.addEventListener("click", sendAlert());
+
+function sendAlert() {
+  alert(alertText);
 }

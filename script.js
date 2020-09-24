69653791fe10d731049f8dc3086aecd4e2871a53
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-09-24 16:53:31 -0300

    code climate

---
+++ script.js
@@ -0,0 +1,5 @@
+botaoLogin = document.querySelector("#button-login");
+botaoLogin.addEventListener("click", function(){
+  emailTelefone = document.querySelector("#user-email-phone");
+  alert(emailTelefone.value);
+});

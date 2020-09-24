07a83c5274485650ebce06034105a2951a688c88
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-09-24 18:43:19 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const entrar = document.getElementById('button-login');
 const emailPhone = document.getElementById('user-email-phone');
+const inputs = document.querySelectorAll('.forms input');
+const register = document.getElementById('facebook-register');
+const invalidValues = document.getElementById('invalid');
 
 entrar.addEventListener('click', () => {
   alert(emailPhone.value);
 });
+
+function validateInputs() {
+  for (let i = 0; i < inputs.length; i += 1) {
+    if (!inputs[i].value) {
+      invalidValues.innerText = 'Campos inválidos';
+    }
+  }
+}
+
+register.addEventListener('click', validateInputs);

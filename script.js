94c3e377c890cd616a3ac6575074e7f85db4d1b3
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-24 16:32:32 -0300

    Arrumando requisitos

---
+++ script.js
@@ -0,0 +1,6 @@
+let button = document.querySelector('#button-login');
+let input = document.querySelector('#user-email-phone');
+
+button.addEventListener('click', function () {
+    alert(input.value);
+});

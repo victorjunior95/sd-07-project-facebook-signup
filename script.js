2f67d98804a8d4498df7c14049b98c90d80ddf64
samantabelow <samanta.below@gmail.com>
2020-09-24 17:36:23 -0300

    Ajustes pro codeclimate

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-let buttonLogin = document.getElementById('button-login');
-buttonLogin.addEventListener('click', function() {
-    let value = document.getElementById('user-email-phone').value;
-    alert(value);
-})
+const buttonLogin = document.getElementById("button-login");
+buttonLogin.addEventListener("click", function () {
+  const value = document.getElementById("user-email-phone").value;
+  alert(value);
+});
 
-new window.JustValidate('form-register', {
-    rules: {
-        firstname: {
-            required: true
-        },
-        lastname: {
-            required: true
-        },
-        phone_email: {
-            required: true
-        },
-        password: {
-            required: true
-        },
-        birthdate: {
-            required: true
-        },
-        genero: {
-            required: true
-        }
-    }
+window.JustValidate("form-register", {
+  rules: {
+    firstname: {
+      required: true,
+    },
+    lastname: {
+      required: true,
+    },
+    phone_email: {
+      required: true,
+    },
+    password: {
+      required: true,
+    },
+    birthdate: {
+      required: true,
+    },
+    genero: {
+      required: true,
+    },
+  },
 });

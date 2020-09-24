826fdd373c81cd2908fd047d54c2f309d8cf6c57
vini-lima <vini20.henrique@gmail.com>
2020-09-24 18:42:57 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 const user = document.querySelector('#user-email-phone');
 const button = document.querySelector('#button-login');
+const firstname = document.querySelector('#firstname');
+const lastname = document.querySelector('#lastname');
+const phoneEmail = document.querySelector('#phone-email');
+const password = document.querySelector('#password');
+const birthdate = document.querySelector('#birthdate');
+const female = document.querySelector('#female');
+const male = document.querySelector('#male');
+const other = document.querySelector('#other');
+const facebookRegister = document.querySelector('#facebook-register');
 
 button.addEventListener('click', function () {
   alert(user.value);
+});
+
+facebookRegister.addEventListener('click', function () {
+  let check = false;
+  let array = [firstname, lastname, phoneEmail, password, birthdate];
+  let arrayGender = [female, male, other];
+  for (let index = 0; index < arrayGender.length; index += 1) {
+    if (arrayGender[index].checked == true) {
+      check = true;
+    }
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index].value.length === 0) {
+      check = false;
+    }
+  }
+  if (check === false) {
+    alert('Campos inválidos');
+  }
 });

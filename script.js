c91667bc6ed9d996e23259cd7cbe1435d8d7ac1e
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2020-09-24 18:20:21 -0300

    resolvendo alguns issues

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttonLogin = document.getElementById('button-login');
 const userPmailPhone = document.getElementById('user-email-phone');
 
-buttonLogin.addEventListener('click', function() {
-    alert(userPmailPhone.value)
-})
+buttonLogin.addEventListener('click', function () {
+  alert(userPmailPhone.value);
+});

d808a33810b0d628510fc06294dae5a834284ee0
samantabelow <samanta.below@gmail.com>
2020-09-24 17:30:22 -0300

    Até requisito 18 completo

---
+++ script.js
@@ -0,0 +1,28 @@
+let buttonLogin = document.getElementById('button-login');
+buttonLogin.addEventListener('click', function() {
+    let value = document.getElementById('user-email-phone').value;
+    alert(value);
+})
+
+new window.JustValidate('form-register', {
+    rules: {
+        firstname: {
+            required: true
+        },
+        lastname: {
+            required: true
+        },
+        phone_email: {
+            required: true
+        },
+        password: {
+            required: true
+        },
+        birthdate: {
+            required: true
+        },
+        genero: {
+            required: true
+        }
+    }
+});

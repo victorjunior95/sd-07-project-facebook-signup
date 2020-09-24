8bbbddf7b470d6228c4370cb3c200ec9285cb3d5
Danwhat <daanwhat@gmail.com>
2020-09-24 19:10:20 -0300

    requisito 18

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const female = document.querySelector('#female');
 const male = document.querySelector('#male');
 const other = document.querySelector('#other');
 const facebookRegister = document.querySelector('#facebook-register');
+const answer = document.querySelector('#answer');
 
 button.addEventListener('click', function () {
   alert(user.value);
@@ -19,16 +20,10 @@ facebookRegister.addEventListener('click', function () {
   const array = [firstname, lastname, phoneEmail, password, birthdate];
   const arrayGender = [female, male, other];
   for (let index = 0; index < arrayGender.length; index += 1) {
-    if (arrayGender[index].checked === true) {
-      check = true;
-    }
+    if (arrayGender[index].checked === true) check = true;
   }
   for (let index = 0; index < array.length; index += 1) {
-    if (array[index].value.length === 0) {
-      check = false;
-    }
+    if (array[index].value.length === 0) check = false;
   }
-  if (check === false) {
-    alert('Campos inválidos');
-  }
+  if (check === false) answer.style.display = 'block';
 });

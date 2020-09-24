e91b978adb3825bc2a81c083c3f5c1328184e5e0
Wendrick <wendrick.b.c1@gmail.com>
2020-09-24 19:06:47 -0300

    Corrigindo o CC part 3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const buttonEnter = document.querySelector('#button-login');
 buttonEnter.addEventListener('click', function () {
-  const emailPhone = document.querySelector('#user-email-phone'); 
+  const emailPhone = document.querySelector('#user-email-phone');
   alert(emailPhone.value);
 });

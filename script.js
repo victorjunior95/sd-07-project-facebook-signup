6e25c3ba7cad53479155c1c5dd5b7facf76e718b
Carlos Souza <carlos.pessoal@hotmail.com>
2020-09-24 18:31:38 -0300

    Ajustes do avaliador

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+function checkForm() {
+  let error = false;
+  document.querySelectorAll('.cadastro input').forEach((input) => {
+    if (input.type !== 'radio') {
+      if (input.value === '') {
+        error = true;
+      }
+    }
+  });
+
+  if (error) {
+    document.querySelector('.error').style.display = 'block';
+  }
+}
+
 document.body.onload = function () {
-  document.querySelector('form').addEventListener('submit', function (event) {
-    event.preventDefault();
+  document.querySelectorAll('form').forEach((form) => {
+    form.addEventListener('submit', (e) => e.preventDefault());
   });
 
   document.getElementById('button-login').addEventListener('click', function () {
     alert(document.getElementById('user-email-phone').value);
   });
+
+  document.querySelector('#facebook-register').addEventListener('click', checkForm);
 };

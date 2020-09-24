e3f1ce9892e66a074c1e2eb8323562ce430615a1
Camila Valentim <camilaifpb@hotmail.com>
2020-09-24 20:23:49 -0300

    adicionando o alert

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const buttonLog = document.querySelector('#button-login');
 
-buttonLog.addEventListener('click', function () {
-  alert('Email ou telefone');
+buttonLog.addEventListener('click', function alertMensagem() {
+    const emailOrFone = document.getElementById('user-email-phone');
+    alert(emailOrFone.value);
 
 });

c4f40489435388a34f4967e6cb21f510f57eb322
vanderson-henrique <vhbenedito@gmail.com>
2020-09-24 20:44:29 -0300

    Requisito 19

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ buttonLogin.addEventListener('click', function () {
   const emailTel = document.getElementById('user-email-phone').value;
   window.alert(emailTel);
 });
+
+function createTextField() {
+  const newField = document.createElement('input');
+  const buttonRegister = document.querySelector('#facebook-register');
+  const form = document.querySelector('.open-account');
+  newField.type = 'text';
+  newField.name = 'gender-custom';
+  newField.placeholder = 'Gênero (opcional)';
+  form.insertBefore(newField, buttonRegister);
+}
+
+const buttonCustom = document.getElementById('custom');
+buttonCustom.addEventListener('click', createTextField);
+
+document.getElementById('birthdate').DatePickerX.init({ format: 'dd/mm/yyyy' });

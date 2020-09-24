9c72ff6bf304e9274eaf77d06396da8659eda732
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-24 18:28:40 -0300

    projeto ate requisito 7 sem issues

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttonLogin = document.querySelector('#button-login');
 const emailOrPhone = document.querySelector('#user-email-phone');
-
+// alert('funcionou);
 buttonLogin.addEventListener('click', function alerta() {
   alert(`${emailOrPhone.value}`);
 });

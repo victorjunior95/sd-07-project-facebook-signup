55e4e1b670ce0b7009d3995b4b7b64e8e205657b
luc-zago <luc.zago@hotmail.com>
2020-09-24 19:11:29 -0300

    Adicionada função que verifica se o botão radio foi selecionado

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const logBt = document.getElementById('button-login');
 logBt.addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
 });
+
+function checkRadio () {
+  let counter = 0;
+  const genderList = document.getElementsByName('gender');
+  for (let index = 0; index < genderList.length; index += 1) {
+    if (genderList[index].checked === false) {
+      counter += 1;
+    }
+  }
+  if (counter === 3) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validateForm () {
+  let inputArray = document.getElementsByTagName('input');
+}

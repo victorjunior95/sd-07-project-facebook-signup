a71c27ddc0820bd519158c7aa07787f45fc8ad93
kramersilva <kramerscs@gmail.com>
2020-09-24 17:16:29 -0300

    requisitos 6 ao 8

---
+++ script.js
@@ -0,0 +1,6 @@
+const buttonLogin = document.getElementById('button-login');
+
+buttonLogin.addEventListener('click', function () {
+  const emailInput = document.getElementById('user-email-phone');
+  alert(emailInput.value);
+});

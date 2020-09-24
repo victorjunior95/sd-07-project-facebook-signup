934c7f1eeca7057580017b7bf77b73ee3f98f24f
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-24 16:36:03 -0300

    Corrigindo CC

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let button = document.querySelector('#button-login');
-let input = document.querySelector('#user-email-phone');
+const button = document.querySelector('#button-login');
+const input = document.querySelector('#user-email-phone');
 
 button.addEventListener('click', function () {
-    alert(input.value);
+  alert(input.value);
 });

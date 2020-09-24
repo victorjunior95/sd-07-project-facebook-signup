ad6c338b2036234e365372122dd7c992665d0203
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-09-24 19:49:56 -0300

    CC

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 /* validate form empty */
+function verifyEmpty(values) {
+  for (let value of values) {
+    if (value === '') return true
+  }
+}
 const btnValidate = document.getElementById('facebook-register');
 btnValidate.addEventListener('click', function () {
-  const firstname = document.forms['form-facebook']['firstname'].value;
-  const lastname = document.forms['form-facebook']['lastname'].value;
-  const phone_email = document.forms['form-facebook']['phone_email'].value;
-  const password = document.forms['form-facebook']['password'].value;
-  const birthdate = document.forms['form-facebook']['birthdate'].value;
-  const gender = document.forms['form-facebook']['gender'].value;
+  const firstname = document.forms['form-facebook'].firstname.value;
+  const lastname = document.forms['form-facebook'].lastname.value;
+  const phone_email = document.forms['form-facebook'].phone_email.value;
+  const password = document.forms['form-facebook'].password.value;
+  const birthdate = document.forms['form-facebook'].birthdate.value;
+  const gender = document.forms['form-facebook'].gender.value;
 
-  if (firstname == '' || lastname == '' || phone_email == '' || password == '' || birthdate == '' || gender == '') {
+  console.log(document.forms['form-facebook'].firstname.value);
+
+  if (verifyEmpty([firstname, lastname, phone_email, password, birthdate, gender])) {
     const p = document.getElementsByClassName('invalided')[0];
     p.innerText = "Campos inválidos";
   }

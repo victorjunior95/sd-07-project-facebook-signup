16b4170f1b4f3f24cc87fc5754416fa4beb19ee9
Carol Andrade <a.carolg1@gmail.com>
2020-09-24 19:16:14 -0300

    requisito18 sub1

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ buttonLogin.addEventListener('click', function () {
   const nameInput = document.getElementById('user-email-phone').value;
   alert(`${nameInput}`);
 });
+
+new window.JustValidate('.js-form', {
+    rules: {
+        firstname: {
+            required: true,
+        },
+        lastname: {
+            required: true,
+        },
+        phone_email: {
+            required: true,
+        },
+        password: {
+            required: true,
+        },
+        birthdate: {
+            required: true,
+        },
+        radio: {
+            required: true,
+        },
+    },
+    messages: {
+        firstname: 'Campos inválidos',
+        lastname: 'Campos inválidos',
+        phone_email: 'Campos inválidos',
+        password: 'Campos inválidos',
+        birthdate: 'Campos inválidos',
+        radio: 'Campos inválidos'
+      },
+})

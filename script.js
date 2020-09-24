4e5640b49ba55a529da63ac2b9bcd55976ab291e
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-24 18:31:13 -0300

    Teste

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // const button = document.getElementById('button-login');
+const other = document.getElementById('personalizado');
+
+other.addEventListener('change', () => {
+  const father = document.getElementById('signup-form');
+  const customGender = document.createElement('input');
+  customGender.type = 'text';
+  customGender.name = 'gender-custom';
+  customGender.placeholder = 'Gênero (opcional)';
+  customGender.id = 'gender-custom';
+  customGender.style.display = 'flex';
+  customGender.style.width = '100%';
+  father.appendChild(customGender);
+  father.insertBefore(customGender, father.childNodes[14]);
+});

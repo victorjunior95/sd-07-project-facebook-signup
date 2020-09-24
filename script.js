6ca5e72b92a32d2e9656cb63f7e12ff1b02e5975
renatomak <renato.mark.silva@gmail.com>
2020-09-24 19:17:14 -0300

    Rquisito 7 - adicionada uma escuta ao button

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttonLogin = document.getElementById('button-login');
+const userPassword = document.getElementById('user-password');
+const userEmailPhone = document.getElementById('user-email-phone');
+
+buttonLogin.addEventListener('click', function () {
+  alert('Email ou telefone');
+})

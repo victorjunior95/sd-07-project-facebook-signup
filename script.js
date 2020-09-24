3335ad0e6426eedad4f9f7b69af8e1b6c39e36e8
Wendrick <wendrick.b.c1@gmail.com>
2020-09-24 19:00:55 -0300

    Corrigindo o CC

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let buttonEnter = document.querySelector("#button-login");
-buttonEnter.addEventListener("click", function() {
-    let emailPhone = document.querySelector("#user-email-phone");
-    let password = document.querySelector("#user-passowrd");
-    alert(emailPhone.value);
+const buttonEnter = document.querySelector('#button-login');
+buttonEnter.addEventListener( 'click', function() {
+  const emailPhone = document.querySelector('#user-email-phone');
+  const password = document.querySelector('#user-passowrd');
+  alert(emailPhone.value);
 });

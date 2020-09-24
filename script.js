5866749425e65389ae5191339cbf3cbfe0b73559
Lets <letsferreiratx@gmail.com>
2020-09-24 17:16:36 -0300

    criando button e alert

---
+++ script.js
@@ -0,0 +1,5 @@
+const buttonLogin = document.querySelector('#button-login');
+buttonLogin.addEventListener('click', function(){
+  const email = document.getElementById('user-email-phone').value;
+  alert(email);
+})

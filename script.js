dd8d810344a4bcd06260bd117361ce36167d5961
Arthur Nunes <coletivosuburbano@gmail.com>
2020-09-24 18:00:22 -0300

    ex 1,2,3,4,5,6,7

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+let buttonLogin = document.querySelector('#button-login');
+let emailOrTel = document.querySelector('#user-email-phone');
 
+buttonLogin.addEventListener('click', function (event) {
+  event.preventDefault();
+  alert(emailOrTel.value);
+});

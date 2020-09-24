03faec10ebc0cbfa4b6aa863439030ada3366f35
brendalima <brenda.c.lima@poli.ufrj.br>
2020-09-24 19:33:16 -0300

    consertando clarity issues no CC

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const submitBt = document.querySelector("#button-login");
+const submitBt = document.querySelector('#button-login');
 
-submitBt.addEventListener('click', function() {
-  const getUser = document.querySelector("#user-email-phone").value;
+submitBt.addEventListener('click', function () {
+  const getUser = document.querySelector('#user-email-phone').value;
   alert(getUser);
 });

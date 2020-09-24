404c772e4fd2b4e9979cb702e6ed5fc4c2829bf0
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-09-24 19:39:02 -0300

    CC

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 /* validate form empty */
 const btnValidate = document.getElementById('facebook-register');
-btnValidate.addEventListener('click', function(){
-  let firstname = document.forms['form-facebook']['firstname'].value;
-  let lastname = document.forms['form-facebook']['lastname'].value;
-  let phone_email = document.forms['form-facebook']['phone_email'].value;
-  let password = document.forms['form-facebook']['password'].value;
-  let birthdate = document.forms['form-facebook']['birthdate'].value;
-  let gender = document.forms['form-facebook']['gender'].value;
+btnValidate.addEventListener('click', function () {
+  const firstname = document.forms['form-facebook']['firstname'].value;
+  const lastname = document.forms['form-facebook']['lastname'].value;
+  const phone_email = document.forms['form-facebook']['phone_email'].value;
+  const password = document.forms['form-facebook']['password'].value;
+  const birthdate = document.forms['form-facebook']['birthdate'].value;
+  const gender = document.forms['form-facebook']['gender'].value;
 
   if (firstname == '' || lastname == '' || phone_email == '' || password == '' || birthdate == '' || gender == '') {
-    let p = document.getElementsByClassName('invalided')[0];
+    const p = document.getElementsByClassName('invalided')[0];
     p.innerText = "Campos inválidos";
   }
 });

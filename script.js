b7d842f87254aae30987ff0585aab09f7d47b2b6
Cainan Coutinho <cainan6697@gmail.com>
2020-09-24 20:36:19 -0300

    Requisito 7, form control com botao entrar

---
+++ script.js
@@ -0,0 +1,6 @@
+const buttonLogin = document.querySelector('#button-login');
+
+buttonLogin.addEventListener('click', function () {
+  const campoEmail = document.querySelector('#user-email-phone').value;
+  alert(campoEmail);
+});

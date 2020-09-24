fc70ca6a0fd28c31ee3b7f1be5742fe89e141c5b
Fernando <nando.neves@gmail.com>
2020-09-24 18:49:25 -0300

    gender

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 // Colocar/Retirar a caixa de texto de gênero-personalizado;
 const personalizado = document.getElementById('personalizado');
-const select = document.getElementById('genero-personalizado');
-const masculino = document.getElementById('masculino');
-const feminino = document.getElementById('feminino');
+// const select = document.getElementById('genero-personalizado');
+// const masculino = document.getElementById('masculino');
+// const feminino = document.getElementById('feminino');
+const genero = document.getElementById('gender');
 personalizado.addEventListener('change', function () {
+//   document.getElementById('gender-custom').innerText = '';
+  const select = document.createElement('input');
   select.style.display = 'block';
+  select.placeholder = 'Gênero (opcional)';
+  select.name = 'gender-custom';
+  select.id = 'gender-custom';
+  genero.appendChild(select);
 });
-masculino.addEventListener('change', function () {
-  select.style.display = 'none';
-});
-feminino.addEventListener('change', function () {
-  select.style.display = 'none';
-});
+// masculino.addEventListener('change', function () {
+//   select.style.display = 'none';
+// });
+// feminino.addEventListener('change', function () {
+//   select.style.display = 'none';
+// });
 
 // Alerta do botão login
 const botaoLogin = document.getElementById('button-login');

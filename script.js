cb8b1f78823be8a275a3efe41b693f45c414b42b
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-24 17:17:28 -0300

    Requisitos gerais

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const button = document.querySelector('#button-login');
+const buttonLogin = document.querySelector('#button-login');
 const input = document.querySelector('#user-email-phone');
 
-button.addEventListener('click', function () {
+buttonLogin.addEventListener('click', function () {
   alert(input.value);
 });
+
+const buttonRegister = document.querySelector('#facebook-register');
+buttonRegister.addEventListener('click', function(){
+    let inputs = document.querySelectorAll('.main-form');
+})

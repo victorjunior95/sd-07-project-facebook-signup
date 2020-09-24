7a945ea45c3f44e3e9c21b9289f4af96788ff7be
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-09-24 18:25:26 -0300

    requisito 7

---
+++ script.js
@@ -0,0 +1,5 @@
+let btButtonLogin = document.getElementById('button-login');
+let inpUserEmailPhone = document.getElementById('user-email-phone');
+btButtonLogin.addEventListener('click', function () {
+    alert(inpUserEmailPhone.value)   
+});

348bb3fab501470a8cdce2a4a7e8378e68842e52
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-09-24 19:52:51 -0300

    requisito 19

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const emailPhone = document.getElementById('user-email-phone');
 const inputs = document.querySelectorAll('.forms input');
 const register = document.getElementById('facebook-register');
 const invalidValues = document.getElementById('invalid');
+const ifChecked = document.getElementById('genero-personalizado');
 
 entrar.addEventListener('click', () => {
   alert(emailPhone.value);
@@ -17,3 +18,11 @@ function validateInputs() {
 }
 
 register.addEventListener('click', validateInputs);
+ifChecked.addEventListener('click', () => {
+  if (ifChecked.checked) {
+    const newInput = document.createElement('input');
+    newInput.type = 'text';
+    newInput.name = 'gender-custom';
+    invalidValues.appendChild(newInput);
+  }
+});

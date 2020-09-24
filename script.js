67ba5af5834795d02e63d6de051703ffa9df9046
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-09-24 20:45:16 -0300

    Code Climate Ajustes - 24/09/2020

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 function alertLogin() {
-  const buttonForm = document.querySelector("#button-login");
-  const loginInput = document.querySelector("#user-email-phone");
+  const buttonForm = document.querySelector('#button-login');
+  const loginInput = document.querySelector('#user-email-phone');
 
-  buttonForm.addEventListener("click", function () {
+  buttonForm.addEventListener('click', function () {
     alert(loginInput.value);
   });
 }
 
 function registerAlert() {
-  const rightContentInputs = document.querySelector("#sign-up-container").querySelectorAll("input");
-  const buttonRegister = document.querySelector("#facebook-register");
+  const rightContentInputs = document.querySelector('#sign-up-container').querySelectorAll('input');
+  const buttonRegister = document.querySelector('#facebook-register');
 
-  buttonRegister.addEventListener("click", function () {
+  buttonRegister.addEventListener('click', function () {
     rightContentInputs.forEach((item) => {
-      let requiredContainer = document.querySelector(".required-alert");
+      const requiredContainer = document.querySelector('.required-alert');
 
-      item.required = "required";
-      if (item.value == "") {
-        requiredContainer.innerText = "Campos inválidos";
+      item.required = 'required';
+      if (item.value === '') {
+        requiredContainer.innerText = 'Campos inválidos';
       }
     });
   });
 }
 
 function createInputGender() {
-  const genderOptions = document.querySelector("#other");
-  const genderContainer = document.querySelector(".gender-container");
+  const genderOptions = document.querySelector('#other');
+  const genderContainer = document.querySelector('.gender-container');
 
-  genderOptions.addEventListener("click", function () {
-    let divOtherGender = document.createElement("div");
-    let inputOtherGender = document.createElement("input");
+  genderOptions.addEventListener('click', function () {
+    const divOtherGender = document.createElement('div');
+    const inputOtherGender = document.createElement('input');
 
-    inputOtherGender.name = "gender-custom";
-    inputOtherGender.placeholder = "Gênero (opcional)";
-    inputOtherGender.required = "required";
+    inputOtherGender.name = 'gender-custom';
+    inputOtherGender.placeholder = 'Gênero (opcional)';
+    inputOtherGender.required = 'required';
 
     divOtherGender.appendChild(inputOtherGender);
     genderContainer.appendChild(divOtherGender);
@@ -45,4 +45,4 @@ window.onload = function () {
   alertLogin();
   createInputGender();
   registerAlert();
-}
+};

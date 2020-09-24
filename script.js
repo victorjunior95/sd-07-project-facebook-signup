5db60d7e187003e1a4d04d4ccb9c8698165930eb
TIAGO <tiagoesdrasvieira@gmail.com>
2020-09-24 17:39:32 -0300

    Requisito 7 - Fix alert

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ const loginBtn = document.querySelector('#button-login');
 loginBtn.addEventListener('click', function (event) {
   event.preventDefault();
   const emailInput = document.querySelector('#user-email-phone').value;
-  const passwordInput = document.querySelector('#user-password').value;
-  alert(`${emailInput} ${passwordInput}`);
+  alert(`${emailInput}`);
 });

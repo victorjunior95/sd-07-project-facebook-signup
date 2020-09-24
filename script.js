10735694f744358514331b4ac135830030b84a2e
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-09-24 17:23:25 -0300

    requisition 7

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Comentando para não tiltar o CC
-// const enterButton = document.querySelector('#button-login');
-// const inputValue = document.querySelector('#user-email-phone');
+const enterButton = document.querySelector('#button-login');
+const inputValue = document.querySelector('#user-email-phone');
 
-// enterButton.addEventListener('click', function () {
-//   alert(inputValue.value);
-// }
+enterButton.addEventListener('click', function () {
+  alert(inputValue.value);
+});

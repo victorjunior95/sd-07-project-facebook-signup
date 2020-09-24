5112a3a58ba1d9f34a8912cba86707bef420b398
lukeCruz <enio.cruz@yahoo.com.br>
2020-09-24 19:09:06 -0300

    index style script

---
+++ script.js
@@ -0,0 +1,7 @@
+const enterButton = document.querySelector('#button-login');
+const Inputvalue = document.querySelector('user-email-phone')
+
+
+enterButton.addEventListener('click',function() {
+alert(Inputvalue)
+});

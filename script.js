8a3d9aed7d8e24887b1373eb7689443f627196a1
Pedro MF <pedromarques1011@gmail.com>
2020-09-24 19:15:10 -0300

    corrigindo climate

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const inputs = document.querySelectorAll('.right-content input')
+const inputs = document.querySelectorAll('.right-content input');
 
 document.querySelector('#button-login').addEventListener('click', () => {
   alert(document.querySelector('#user-email-phone').value);
 });
 
-document.querySelector("#facebook-register").addEventListener('click', (event) => {
-  for (let i = 0; i < inputs.length; i += 1) {
-    if (inputs[i].value == '') {
-      event.preventDefault()
-      document.querySelector('.handle-error').innerHTML = 'Campo inválido'
+document
+  .querySelector('#facebook-register')
+  .addEventListener('click', (event) => {
+    for (let i = 0; i < inputs.length; i += 1) {
+      if (inputs[i].value === '') {
+        event.preventDefault();
+        document.querySelector('.handle-error').innerHTML = 'Campo inválido';
+      }
     }
-  }
-})
+  });

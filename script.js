1c54c37599674c996816ddd4d4aeb1e62fa1b5bd
Galaraz <jvictor.si.2013@gmail.com>
2020-09-25 12:47:55 -0300

    code-climate o retorno45

--- script.js
+++ script.js
@@ -1,5 +1,65 @@
-const button = document.getElementById('button-login');
+function validar() {
+  // Leitura dos dados
+  const userLogin = document.getElementById("user-email-phone").value;
+  const userPassord = document.getElementById("user-password").value;
 
-button.addEventListener('click', function () {
-  alert('Email ou telefone');
-});
+  let mensagem = "";
+  // Comparações
+  if (userLogin == "") {
+    mensagem += "Campo nome é obrigatório!!!\n";
+  }
+  if (userPassord == "") {
+    mensagem += "Campo e-mail é obrigatório!!!\n";
+  }
+
+  if (mensagem != "") {
+    alert("Email ou telefone");
+  }
+
+  if (mensagem == "") {
+    alert("login efetuado com sucesso ");
+  }
+}
+function validarNovoUsuario() {
+  let firstName = document.getElementById("firstname").value;
+  let lastName = document.getElementById("lastname").value;
+  let email = document.getElementById("phone_email").value;
+  let password = document.getElementById("password").value;
+  let genero = document.querySelector("[type=radio]:checked");
+  let dataNasc = document.getElementById("birthdate").value;
+
+  let mensagem = "";
+
+  // Comparações
+  if (firstName == "") {
+    mensagem += "Campo nome é obrigatório!!!\n";
+  }
+
+  if (lastName == "") {
+    mensagem += "Campo sobrenome é obrigatório!!!\n";
+  }
+
+  if (email == null) {
+    mensagem += "Selecione pelo menos um curso!!!\n";
+  }
+
+  if (password == "") {
+    mensagem += "Selecione o Password!!!\n";
+  }
+
+  if (genero == null) {
+    mensagem += "Selecione o genero!!!\n";
+  }
+
+  if (dataNasc == "") {
+    mensagem += "Selecione a data de nascimento!!!\n";
+  }
+
+  if (mensagem != "") {
+    alert(mensagem);
+  }
+
+  if (mensagem == "") {
+    alert("cadastro efetuado com sucesso ");
+  }
+}

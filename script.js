42df367b1229f6d1dd2a094ff5bd5d9e6671dc13
Carol Andrade <a.carolg1@gmail.com>
2020-09-24 21:35:39 -0300

    requisito19 sub1

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ buttonLogin.addEventListener('click', function () {
   alert(`${nameInput}`);
 });
 
+const persInput = document.getElementById('personalizado');
+persInput.addEventListener('click', function () {
+  const choiceGender = document.createElement('input');
+  choiceGender.className = 'personalizado';
+  choiceGender.type = 'text';
+  choiceGender.placeholder = 'Gênero (opcional)';
+  choiceGender.name = 'gender-custom';
+  const divGender = document.getElementById('pers-input');
+  divGender.appendChild(choiceGender);
+});
+
 //  solução com a ajuda da colega Samata Below
 const validate = new window.JustValidate('.js-form', {
   rules: {

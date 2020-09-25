7339d834c0b8edd1380ccf08605b4bc0faa2dc72
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-09-25 00:34:16 -0300

    Resolvendo requisito 19

--- script.js
+++ script.js
@@ -4,13 +4,19 @@ function alertEmailOrPhone() {
   alert(emailOrPhone.value);
 }
 
+const genderCustom = document.querySelector('.gender-custom');
+
+function createCustom() {
+  genderCustom.innerHTML = '';
+  const input = document.createElement('input');
+  input.setAttribute('name', 'gender-custom');
+  input.placeholder = 'Gênero (opcional)';
+  genderCustom.appendChild(input);
+}
+
 const authenticationButton = document.getElementById('button-login');
-
-authenticationButton.addEventListener('click', alertEmailOrPhone);
-
 const buttonRadio = document.querySelector('#personalizado');
-const genderCustom = document.getElementById('gender-custom');
 
-buttonRadio.addEventListener('click', function () {
-  genderCustom.setAttribute('type', 'text');
-});
+authenticationButton.addEventListener('change', alertEmailOrPhone);
+
+buttonRadio.addEventListener('click', createCustom);

13f452d5b4e1ba7a94a16cb9aa45be15be589e9f
ericvini <ericcobain3@gmail.com>
2020-09-25 20:56:50 -0300

    Adicionei o alert no requisito 7

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputUserEmailPhone=document.getElementById('user-email-phone');
+const btn=document.getElementById('button-login');
+btn.addEventListener('click',()=>{
+
+alert(inputUserEmailPhone.value);
+
+})

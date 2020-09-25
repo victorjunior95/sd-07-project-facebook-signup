e047c8020313d9a0c696d5b17bf5203a58b05ab2
Breno Alves <brenoalv3ss@gmail.com>
2020-09-25 16:27:11 -0300

    Solução quesito 19

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const button = document.getElementById('button-login');
 button.addEventListener('click', function () {
   alert(email.value);
 });
+
+function addText() {
+  const sexo = document.getElementById('genero');
+  let input = document.createElement('INPUT');
+  input.setAttribute("type", "text");
+  input.setAttribute("name", 'gender-custom');
+  input.setAttribute('placehold', 'Gênero (Opcional)');
+  input.setAttribute("onkeyup", "lsRef()");
+  sexo.appendChild(input);
+}

cf6a0dbf5d0fb736e4a181deefcf3d5cac8600b2
Vitor Rodrigues <vitor_rc@outlook.com>
2020-09-25 14:00:16 -0300

    Commit para avaliação

--- script.js
+++ script.js
@@ -54,25 +54,10 @@ function personalizadoSelecionado() {
   campoPersonalizado.placeholder = 'Gênero (opcional)';
   campoPersonalizado.id = 'idgenero';
   divPesonalizado.appendChild(campoPersonalizado);
-  // Inicio Gambiarra
-  // const campoPersonalizado2 = document.createElement('input');
-  // campoPersonalizado2.name = 'gender';
-  // campoPersonalizado2.placeholder = 'Gênero (opcional)';
-  // divPesonalizado.appendChild(campoPersonalizado2);
-  // Fim Gambiarra
-  // const idgenero = document.getElementById('idgenero');
-  // idgenero.style.display = '';
 }
 
 function removerDivPersonalizado() {
   document.querySelector('#pesonalizado-selecionado').innerHTML = '';
-  // Gambiarra
-  // const idgenero = document.getElementById('idgenero');
-  // if (idgenero != null) {
-  //   idgenero.remove();
-  // }
-  // const idgenero = document.getElementById('idgenero');
-  // idgenero.style.display = 'none';
 }
 
 const personalizadoForm = document.querySelector('#personalizado');

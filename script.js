92ae0cbb8cadd41aea566d3801ca66029ddd9f58
Julio Kauer <juliokauer@gmail.com>
2020-09-25 12:17:04 -0300

    Desisti do JustValidate

--- script.js
+++ script.js
@@ -5,25 +5,3 @@ buttonLogin.addEventListener('click', function (event) {
   event.preventDefault();
   alert(emailOrTel.value);
 });
-
-window.onload = function () {
-  const validate = new window.JustValidate('.form-signup', {
-    rules: {
-      firstname: {
-        required: true,
-      },
-      lastname: {
-        required: true,
-      },
-    },
-    messages: 'Campos inválidos'
-  });
-
-  // POG - Programação Orientada a Gambiarra
-  // Dica Samanta Below
-  // https://trybecourse.slack.com/archives/C017W4EDD4K/p1600990300157300
-  function letCodeClimateHappy() {
-    return validate;
-  }
-  letCodeClimateHappy();
-};

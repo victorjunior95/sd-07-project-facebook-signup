d31036f1b2bf8ad4b90d70f16553a13fe4484d39
Melissa Gomes <mel.ge171201@gmail.com>
2020-09-25 12:29:26 -0300

    adicionando outros requisitos

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ buttonLogin.addEventListener('click', function () {
   const emailInput = document.getElementById('user-email-phone');
   alert(emailInput.value);
 });
+
+const buttonRegister = document.getElementById('facebook-register');
+
+buttonRegister.addEventListener('click', function () {
+  event.preventDefault();
+  const inputItems = document.getElementsByClassName('input-item');
+  for (let index = 0; index < inputItems.length; index += 1) {
+    const result = inputItems[index];
+    if (result.value === '') {
+      result.value = 'Campos inválidos';
+    }
+  }
+});
+
+const personal = document.getElementById('personal');
+const form = document.getElementById('form');
+const buttonBox = document.getElementById('button-box');
+personal.addEventListener('click', function () {
+  const newInput = document.createElement('input');
+  form.insertBefore(newInput, buttonBox);
+  newInput.name = 'gender-custom';
+  newInput.placeholder = 'Gênero (opcional)';
+});

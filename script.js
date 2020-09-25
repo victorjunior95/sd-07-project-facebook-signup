79401b31d1e170e11d2620dba325244807adb212
PedroMarqDev <PedroMarqDev@gmail.com>
2020-09-25 12:54:07 -0300

    19/20

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
 const buttonLogin = document.getElementById('button-login');
 const userData = document.getElementById('user-email-phone');
+const formInput = document.querySelectorAll('.right-content form input');
+const registerButton = document.querySelector('#facebook-register');
+const genderInputs = document.querySelectorAll('.gender input');
+let sexvar = '';
+function sexValidate() {
+  const sex = genderInputs;
+  sex[0].addEventListener('click', function () {
+    sexvar = 'Feminino';
+  });
+
+  sex[1].addEventListener('click', function () {
+    sexvar = 'Masculino';
+  });
+
+  sex[2].addEventListener('click', function () {
+    sexvar = 'Outros';
+  });
+}
+sexValidate();
 
 buttonLogin.addEventListener('click', function () {
   alert(userData.value);
+});
+
+function sexValidate() {
+  const sex = genderInputs;
+  sex[0].addEventListener('click', function () {
+    sexvar = 'Feminino';
+  });
+
+  sex[1].addEventListener('click', function () {
+    sexvar = 'Masculino';
+  });
+
+  sex[2].addEventListener('click', function () {
+    sexvar = 'Outros';
+  });
+}
+
+registerButton.addEventListener('click', function (xis) {
+  let count = 0;
+  formInput.forEach((key) => {
+    if (key.value === '') {
+      count += 1;
+    }
+  });
+  if (count >= 1 || sexvar === '') {
+    xis.preventDefault();
+    alert('Campos inválidos');
+  }
 });

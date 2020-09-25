7b0567ca2c7261b4e7866737087301f91c251fb4
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2020-09-25 13:37:09 -0300

    requisito 18 envio 4

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ const emailInput = document.querySelector('#user-email-phone');
 const registerFB = document.querySelector('#facebook-register');
 const tagFormCreate = document.querySelector('.user-create');
 const inputFormCreate = document.querySelectorAll('.create-form');
+let createFormValid = false;
 
 buttonLogin.addEventListener('click', function () {
   alert(emailInput.value);
 });
 
-function CheckIfAllfieldsAreFilled() {
+function showAllInformations() {
+  if(createFormValid === true) {
+    console.log('DEU CERTO');
+  }
+}
+
+function CheckIfAllfieldsAreFilled(e) {
+   e.preventDefault();
+   console.log(document.querySelector('input[name="gender"]:checked').value);
   for (let index = 0; index < inputFormCreate.length; index += 1) {
     if (inputFormCreate[index].value === '' || inputFormCreate[index].checked === false) {
       const invalidFieldsP = document.createElement('p');
@@ -16,7 +25,9 @@ function CheckIfAllfieldsAreFilled() {
       tagFormCreate.appendChild(invalidFieldsP);
       break;
     }
-  }
+  }  
+  createFormValid = true;
+  showAllInformations();
 }
 
 registerFB.addEventListener('click', CheckIfAllfieldsAreFilled);

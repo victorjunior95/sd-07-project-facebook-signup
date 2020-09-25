e9c8701926da27909aba5935f2767bbb3426f46c
Marcos R. Silva <marcos.1998226@gmail.com>
2020-09-25 11:21:32 -0300

    refluxo

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 
-
-// document.querySelector('.form-control').childNodes[1].addEventListener('click', function () {
-//   alert('Email ou telefone');
-// });
-
 document.querySelector('#button-login').addEventListener('click', () =>
   alert(document.querySelector('#user-email-phone').value));

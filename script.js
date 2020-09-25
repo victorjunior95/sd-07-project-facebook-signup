c2a96d16b2a1b2cfa60a1a853cd99b5f5601ee88
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-09-25 00:53:20 -0300

    Saídas Masculino e Personalizado

--- script.js
+++ script.js
@@ -1,77 +1,97 @@
 function validateInputTextEmpty(inputTexts) {
-  let anyEmpty = false;
-  for (let index = 0; index < inputTexts.length; index += 1) {
-    if (inputTexts[index].value === '' && window.getComputedStyle(inputTexts[index]).display !== 'none') {
-      anyEmpty = true;
-      break;
+    let anyEmpty = false;
+    for (let index = 0; index < inputTexts.length; index += 1) {
+        if (
+            inputTexts[index].value === "" &&
+            window.getComputedStyle(inputTexts[index]).display !== "none"
+        ) {
+            anyEmpty = true;
+            break;
+        }
     }
-  }
-  return anyEmpty;
+    return anyEmpty;
 }
 
 function validateInputRadioUnchecked(inputRadios) {
-  let anyChecked = false;
-  for (let index = 0; index < inputRadios.length; index += 1) {
-    if (inputRadios[index].checked) {
-      anyChecked = true;
-      break;
+    let anyChecked = false;
+    for (let index = 0; index < inputRadios.length; index += 1) {
+        if (inputRadios[index].checked) {
+            anyChecked = true;
+            break;
+        }
     }
-  }
-  return !anyChecked;
+    return !anyChecked;
 }
 
 function changeRightContent() {
-  document.getElementById('divForm').classList.add('invisible');
+    document.getElementById("divForm").classList.add("invisible");
 
-  document.getElementById('welcome-user').innerText = `Olá, ${document.getElementById('firstname').value} ${document.getElementById('lastname').value}`;
-  let genderValue = '';
-  if (document.getElementById('radio-p').checked) {
-    genderValue = document.getElementById('gender-custom').value;
-  } else if (document.getElementById('radio-f').checked) {
-    genderValue = document.getElementById('radio-f').value;
-  } else {
-    genderValue = document.getElementById('radio-m').value;
-  }
-  document.getElementById('info-user').innerHTML = `E-mail ou Telefone: ${document.getElementById('phone_email').value} <br> Data de Nacimento: ${document.getElementById('birthdate').value} <br> Gênero: ${genderValue}`;
+    document.getElementById("welcome-user").innerText = `Olá, ${
+    document.getElementById("firstname").value
+  } ${document.getElementById("lastname").value}`;
+    let genderValue = "";
+    if (document.getElementById("radio-p").checked) {
+        genderValue = document.getElementById("gender-custom").value;
+    } else if (document.getElementById("radio-f").checked) {
+        genderValue = document.getElementById("radio-m").value;
+    } else {
+        genderValue = document.getElementById("radio-p").value;
+    }
+    document.getElementById("info-user").innerHTML = `E-mail ou Telefone: ${
+    document.getElementById("phone_email").value
+  } <br> Data de Nacimento: ${
+    document.getElementById("birthdate").value
+  } <br> Gênero: ${genderValue}`;
 
-  document.getElementById('divSignIn').classList.remove('invisible');
+    document.getElementById("divSignIn").classList.remove("invisible");
 }
 
-
 function stopDefAction(evt) {
-  let validateTextsOK = true;
-  let validateRadiosOK = true;
+    let validateTextsOK = true;
+    let validateRadiosOK = true;
 
-  validateTextsOK = !validateInputTextEmpty(document.querySelectorAll('.facebook-signup input[type=text], input[type=password]'));
-  validateRadiosOK = !validateInputRadioUnchecked(document.querySelectorAll('.facebook-signup input[type=radio]'));
+    validateTextsOK = !validateInputTextEmpty(
+        document.querySelectorAll(
+            ".facebook-signup input[type=text], input[type=password]"
+        )
+    );
+    validateRadiosOK = !validateInputRadioUnchecked(
+        document.querySelectorAll(".facebook-signup input[type=radio]")
+    );
 
-  if (validateTextsOK && validateRadiosOK) {
-    document.getElementById('messagem-erro').innerText = '';
-    changeRightContent();
-  } else {
-    document.getElementById('messagem-erro').innerText = 'Campos inválidos';
-  }
+    if (validateTextsOK && validateRadiosOK) {
+        document.getElementById("messagem-erro").innerText = "";
+        changeRightContent();
+    } else {
+        document.getElementById("messagem-erro").innerText = "Campos inválidos";
+    }
 
-  evt.preventDefault();
+    evt.preventDefault();
 }
 
 function checkRadioGenderCustom() {
-  if (document.getElementById('radio-p').checked) {
-    document.getElementById('gender-custom').classList.remove('invisible');
-  } else {
-    document.getElementById('gender-custom').classList.add('invisible');
-  }
+    if (document.getElementById("radio-p").checked) {
+        document.getElementById("gender-custom").classList.remove("invisible");
+    } else {
+        document.getElementById("gender-custom").classList.add("invisible");
+    }
 }
 
-window.onload = function () {
-  document.getElementById('button-login').addEventListener('click', function () {
-    alert(document.getElementById('user-email-phone').value);
-  });
+window.onload = function() {
+    document
+        .getElementById("button-login")
+        .addEventListener("click", function() {
+            alert(document.getElementById("user-email-phone").value);
+        });
 
-  const inputsRadio = document.querySelectorAll('.facebook-signup input[type=radio]');
-  for (let index = 0; index < inputsRadio.length; index += 1) {
-    inputsRadio[index].addEventListener('click', checkRadioGenderCustom);
-  }
+    const inputsRadio = document.querySelectorAll(
+        ".facebook-signup input[type=radio]"
+    );
+    for (let index = 0; index < inputsRadio.length; index += 1) {
+        inputsRadio[index].addEventListener("click", checkRadioGenderCustom);
+    }
 
-  document.getElementById('facebook-register').addEventListener('click', stopDefAction, false);
+    document
+        .getElementById("facebook-register")
+        .addEventListener("click", stopDefAction, false);
 };

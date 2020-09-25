e4c6d74bc95c22a64f7e9f938ff9c0a261296aec
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-09-25 13:07:25 -0300

    adequando botão de gênero personalizado

--- script.js
+++ script.js
@@ -30,10 +30,11 @@ for (let indexYear = 2020; indexYear >= 1905; indexYear -= 1) {
 
 const customButton = document.querySelector('#custom-button');
 const genderOptions = document.querySelectorAll('.gender-options')[0];
+const customInput = document.querySelector('#custom-input')
 customButton.addEventListener('change', function () {
-  const customInput = document.createElement('input');
-  customInput.setAttribute('type', 'text');
-  customInput.setAttribute('name', 'gender-custom');
-  customInput.setAttribute('placeholder', 'Gênero (opcional)');
-  genderOptions.appendChild(customInput);
+  // if(customButton.isChecked) {
+    customInput.removeAttribute('type', 'hidden');
+    customInput.setAttribute('type', 'text');
+    genderOptions.appendChild(customInput);
+  // };
 });

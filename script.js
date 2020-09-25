3c87fc1f89971b68d77c0add55583b034cf9aa55
murilopereira006 <muriloedu006@gmail.com>
2020-09-25 13:45:46 -0300

    verificando erro no corretor (gender vs gender-custom

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const button = document.querySelector('#button-login');
 const custom = document.querySelector('#Personalizado');
 const labelCustom = document.querySelector('#labelCustom');
+const name = document.querySelector('#name').value;
+const lastName = document.querySelector('#lastname').value;
+const email = document.querySelector('#email').value;
+const birthdate = document.querySelector('#birthdate').value;
+const gender = document.querySelector().value;
+
 button.addEventListener('click', function () {
   const userEmail = document.getElementById('user-email-phone').value;
   alert(userEmail);
@@ -9,7 +15,7 @@ button.addEventListener('click', function () {
 custom.addEventListener('click', function () {
   const campoTexto = document.createElement('input');
   campoTexto.type = 'text';
-  campoTexto.name = 'gender-custom';
+  campoTexto.name = 'gender';
   campoTexto.placeholder = 'Gênero (opcional)';
   labelCustom.appendChild(campoTexto);
 });

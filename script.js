f57e37a56f590cdf90860d6388cd0bab9c6c1c69
moraisgabri <gabriel8azevedo@gmail.com>
2020-09-25 19:13:42 -0300

    formulário até botões de gênero

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ window.onload = function () {
   const loginBtn = document.querySelector('#button-login');
   const userEmailPhone = document.querySelector('#user-email-phone');
 
-  loginBtn.addEventListener('click', function () {
-    alert(userEmailPhone.value);
-  });
+  loginBtn.addEventListener('click', () => alert(userEmailPhone.value));
 };

70b019f3d9584a3f5b5cd797ff86a0eb1c278f01
Vanessa Oliveira <vanessanaara@gmail.com>
2020-09-24 21:25:06 -0300

    Tentativa do requisito 19

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+// Cria um alerta com o "email ou telefone"
 const button = document.querySelector('#button-login');
 const inputEmail = document.querySelector('#user-email-phone');
 
 button.addEventListener('click', function alertEmail() {
   alert(inputEmail.value);
+});
+
+// Cria campo de texto para o gênero personalizado
+const personalizedGender = document.querySelector('#personalized');
+const selectGender = document.querySelector('.select-gender-buttons');
+const inputGender = document.createElement('input');
+
+personalizedGender.addEventListener('click', function () {
+  selectGender.appendChild(inputGender);
+  inputGender.type = 'text';
+  inputGender.placeholder = 'Gênero (opcional)';
+  inputGender.className = 'gender-option';
 });

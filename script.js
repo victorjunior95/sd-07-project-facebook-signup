4607292b7c49df2700fdf80752b019e8a46ddb0e
Luiz F S Mariz <lfsmariz@gmail.com>
2020-09-25 16:28:18 -0300

    requirement 20

--- script.js
+++ script.js
@@ -5,6 +5,28 @@ function plotInvalid() {
   formField.appendChild(msgError);
 }
 
+function plotText() {
+  const formField = document.querySelector('.create-account');
+  const formValues = new FormData(formField);
+  const boxText = document.querySelector('.right-content');
+  boxText.innerHTML = '';
+  boxText.innerHTML = `<p> Olá, ${formValues.get('firstname')} ${formValues.get('lastname')}
+  ${formValues.get('phone_email')} ${formValues.get('birthdate')} ${formValues.get('gender')}
+  </p>`;
+}
+
+function submitAction(verify) {
+  if (verify) {
+    plotText();
+  } else {
+    plotInvalid();
+  }
+}
+
+function handleForm(event) {
+  event.preventDefault();
+}
+
 function createCustomGender() {
   const genderCustom = document.querySelector('.hidden-input');
   genderCustom.innerHTML = '<input name="gender-custom" placeholder="Gênero (opcional)" type="text">';
@@ -14,6 +36,9 @@ const btnLogin = document.querySelector('#button-login');
 const formElements = document.querySelectorAll('.create-account [name]');
 const btnFormSubmit = document.querySelector('#facebook-register');
 const btnCustomGender = document.querySelector('#Personalizado');
+const formCreate = document.querySelector('.create-account');
+
+formCreate.addEventListener('submit', handleForm);
 
 btnCustomGender.addEventListener('click', createCustomGender);
 
@@ -23,10 +48,12 @@ btnLogin.addEventListener('click', () => {
 });
 
 btnFormSubmit.addEventListener('click', () => {
+  let verifyElements = true;
   for (let index = 0; index < formElements.length; index += 1) {
     if (formElements[index].validity.valid === false) {
-      plotInvalid();
+      verifyElements = false;
       break;
     }
   }
+  submitAction(verifyElements);
 });

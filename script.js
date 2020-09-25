dfe07442d540c9eb899b16ab9ec7a51640c49525
renatomak <renato.mark.silva@gmail.com>
2020-09-24 22:34:11 -0300

    refatoraçao do requisito 16 - inclusao dos values CC

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ const userEmailPhone = document.getElementById('user-email-phone');
 buttonLogin.addEventListener('click', function () {
   alert(userEmailPhone.value);
 });
+
+function sexo() {
+  const genderFeminine = document.getElementById('gender-feminine');
+  const genderMale = document.getElementById('gender-male');
+  const genderCustom = document.getElementById('gender-custom');
+  if (genderFeminine) {
+    return 'Feminino'
+  } else if (genderMale) {
+    return 'Masculino'
+  } else if (genderCustom) {
+    return 'Personalizado'
+  }
+}
+
+function validaNome() {
+  const firstName = document.getElementById('firstname');
+  const lastName = document.getElementById('lastname');
+  const phoneEmail = document.getElementById('phone_email');
+  const password = document.getElementById('password');
+  const inputBirthdate = document.getElementById('input-birthdate');
+
+
+
+}

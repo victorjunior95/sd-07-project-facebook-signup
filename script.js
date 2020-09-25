e2f3e15fe14d487f0ef4bad0a10dda9fa3cc1e1e
murilopereira006 <muriloedu006@gmail.com>
2020-09-25 13:33:41 -0300

    campo de texto genero personalizado

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const button = document.querySelector('#button-login');
+const custom = document.querySelector('#Personalizado');
+const labelCustom = document.querySelector('#labelCustom');
 button.addEventListener('click', function () {
   const userEmail = document.getElementById('user-email-phone').value;
   alert(userEmail);
+});
+
+custom.addEventListener('click', function () {
+  const campoTexto = document.createElement('input');
+  campoTexto.type = 'text';
+  campoTexto.name = 'gender-custom';
+  campoTexto.placeholder = 'Gênero (opcional)';
+  labelCustom.appendChild(campoTexto);
 });

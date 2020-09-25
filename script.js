6830e52aa7bdd3a64f440f338d9d94ccdcef70b9
renatomak <renato.mark.silva@gmail.com>
2020-09-24 23:25:28 -0300

    Requisito 18-4

--- script.js
+++ script.js
@@ -8,37 +8,16 @@ buttonLogin.addEventListener('click', function () {
 function sexo() {
   const genderFeminine = document.getElementById('gender-feminine');
   const genderMale = document.getElementById('gender-male');
-  const genderCustom = document.getElementById('gender-custom');
   if (genderFeminine) {
     return 'Feminino';
   } else if (genderMale) {
     return 'Masculino';
   }
-    return 'Personalizado';
+  return 'Personalizado';
 }
 
 function validaCampos() {
-  const firstName = document.getElementById('firstname');
-  const lastName = document.getElementById('lastname');
-  const phoneEmail = document.getElementById('phone_email');
-  const password = document.getElementById('current-password');
-  const inputBirthdate = document.getElementById('input-birthdate');
-
-  let ehValido = false;
-
-  if(firstName && lastName){
-      ehValido = true;
-  }
-  if(phoneEmail && password){
-    ehValido = true;
-  }
-  if(inputBirthdate && sexo()){
-      ehValido = true;
-  }
-
-    if(!ehValido){
-        document.getElementById('messager-user').innerText = 'Campos inválidos';
-    }
+  document.getElementById('messager-user').textContent = 'Campos inválidos';
 }
 
 document.getElementById('facebook-register').addEventListener('click', validaCampos);

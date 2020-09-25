fe2ac5005b481ac80eb686ef3b47f2245e503b9a
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-09-24 22:31:21 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,40 +1,39 @@
-function stopDefAction(evt) {
-  let validateOK = true;
-
-  if (document.getElementById('firstname').value === '') {
-    validateOK = false;
-  }
-  if (document.getElementById('lastname').value === '') {
-    validateOK = false;
-  }
-  if (document.getElementById('phone_email').value === '') {
-    validateOK = false;
-  }
-  if (document.getElementById('password').value === '') {
-    validateOK = false;
-  }
-  if (document.getElementById('birthdate').value === '') {
-    validateOK = false;
-  }
- 
-  const radios = document.getElementsByName('radio-gender');
-  validateRadio = false;
-  for (let index = 0; index < radios.length; index += 1) {
-    if (radios[index].checked) {
-      validateRadio = true;
+function validateInputTextEmpty(inputTexts) {
+  let anyEmpty = false;
+  for (let index = 0; index < inputTexts.length; index += 1) {
+    if (inputTexts[index].value === '') {
+      anyEmpty = true;
+      break;
     }
   }
-  if (!validateRadio) {
-    validateOK = false;
+  return anyEmpty;
+}
+
+function validateInputRadioUnchecked(inputRadios) {
+  let anyChecked = false;
+  for (let index = 0; index < inputRadios.length; index += 1) {
+    if (inputRadios[index].checked) {
+      anyChecked = true;
+      break;
+    }
+  }
+  return !anyChecked;
+}
+
+function stopDefAction(evt) {
+  let validateTextsOK = true;
+  let validateRadiosOK = true;
+
+  validateTextsOK = !validateInputTextEmpty(document.querySelectorAll('.facebook-signup input[type=text], input[type=password]'));
+  validateRadiosOK = !validateInputRadioUnchecked(document.querySelectorAll('.facebook-signup input[type=radio]'));
+
+  if (validateTextsOK && validateRadiosOK) {
+    document.getElementById('messagem-erro').innerText = '';
+  } else {
+    document.getElementById('messagem-erro').innerText = 'Campos inválidos';
   }
 
-  if(!validateOK) {
-    document.getElementById('messagem-erro').innerText = 'Campos inválidos';
-  } else {
-    document.getElementById('messagem-erro').innerText = '';
-  }
-  
-  evt.preventDefault(); 
+  evt.preventDefault();
 }
 
 window.onload = function () {
@@ -44,4 +43,3 @@ window.onload = function () {
 
   document.getElementById('facebook-register').addEventListener('click', stopDefAction, false);
 };
-

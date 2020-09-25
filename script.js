2b7cb81aaa4f513637dc6e4f37520ca6153cfc60
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2020-09-25 16:15:36 -0300

    Ajustando o 7 requisito novamente

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const btnLogin = document.getElementById('button-login');
-const inputText = document.getElementById('user-email-phone-label');
+const inputText = document.querySelector('#user-email-phone');
 
 btnLogin.addEventListener('click', function () {
   alert(inputText.value);

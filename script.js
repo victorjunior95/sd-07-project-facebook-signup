6a298e844611b7361270092342c53efc14b0cf69
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2020-09-25 13:13:15 -0300

    issue

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ document.getElementById('button-login').addEventListener('click', function () {
   const inputText = document.getElementById('user-email-phone');
   alert(inputText.value);
 });
-
 // document.querySelector('facebook-regiset').addEventListener('click', function () {  
 // });

6bab9cda29f709f81311243d13b8aba961a6661d
Alstent <rafaelasltent@gmail.com>
2020-09-25 19:11:16 +0200

    adicionando funcionalidade do campo invalido e do novo input ao clickar na opcao personalizado dos generos

--- script.js
+++ script.js
@@ -1,5 +1,86 @@
 const btnLgn = document.getElementById('button-login');
 const inputEmailPh = document.getElementById('user-email-phone');
+const regForm = document.querySelector('#reg-form')
+const regFormBtn = document.querySelector('#facebook-register')
+const invalidMsg = document.querySelector('#invalid-msg')
+const genderContainer = document.querySelector('#gender-container');
+const genderF = genderContainer.querySelector('input[value="Feminino"]');
+const genderM = genderContainer.querySelector('input[value="Masculino"]');
+const genderCustom = genderContainer.querySelector('input[value="Personalizado"]')
+const genderChecked = genderContainer.querySelector('input[name="genero"]:checked')
+
 btnLgn.addEventListener('click', function () {
   alert(inputEmailPh.value);
 });
+
+function inputCheck() {
+  const camposForm = regForm.getElementsByTagName('input')
+  for (let i = 0; i < camposForm.length; i += 1) {
+    if (camposForm[i].value === '') {
+      invalidMsg.innerHTML = 'Campos inválidos'
+      preventSubmit()
+    }
+  }
+}
+
+function genderCheck() {
+  const genderContainer = document.querySelector('#gender-container');
+  const genderOptions = genderContainer.querySelector('input[name="genero"]:checked');
+  if(genderOptions === null) { 
+    invalidMsg.innerHTML = 'Escolha um gender';
+    preventSubmit();
+  } 
+}
+
+function preventSubmit() {
+  regForm.addEventListener('submit', function (e) {
+    invalidMsg.style.display = 'flex';
+    e.preventDefault();
+  })
+}
+
+function checkForm() {
+  regFormBtn.addEventListener('click', function () {
+    genderCheck();
+    inputCheck();
+  })
+}
+
+function addCustomGenderInput() {
+  genderCustom.addEventListener('click', function () {
+    if(genderChecked !== null) {
+      const customInput = document.querySelector('#custom-input')
+      if(customInput === null) {
+        const newCustomInput = document.createElement('input')
+        newCustomInput.id = 'custom-input'
+        newCustomInput.name = 'gender-custom'
+        newCustomInput.placeholder = 'Genero'
+        newCustomInput.type = 'text' // por default ja é text, mas eu quero especificar na msm
+        genderContainer.appendChild(newCustomInput)
+      }
+    }
+  })
+}
+
+function removeCustomGenderInput() { // pode dar erro com o cypress
+  genderM.addEventListener('click', function () {
+    const customInput = document.querySelector('#custom-input')
+    if(customInput !== null) {
+      const customInput = genderContainer.lastChild
+      genderContainer.removeChild(customInput)
+    }
+  })
+  genderF.addEventListener('click', function () {
+    const customInput = document.querySelector('#custom-input')
+    if(customInput !== null) {
+      const customInput = genderContainer.lastChild
+      genderContainer.removeChild(customInput)
+    }
+  })
+}
+
+window.onload = function () {
+  checkForm();
+  addCustomGenderInput();
+  removeCustomGenderInput()
+}

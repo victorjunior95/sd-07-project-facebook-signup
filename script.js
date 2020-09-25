1a8d4fbfae9b293ea0b8e008e84333df3ab8c531
Bruno Nogueira <nugobruno@gmail.com>
2020-09-25 15:02:03 -0300

    corrigindo erros do css

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const inputValue = document.getElementById('user-email-phone');
 buttonLogin.addEventListener('click', function () {
   alert(inputValue.value);
 });
+
+const buttonCustomGender = document.getElementById('personalizado');
+const genderBox = document.getElementById('gender-box');
+
+buttonCustomGender.addEventListener('click', function () {
+  const genderCustom = document.createElement('input');
+  genderCustom.setAttribute('name', 'gender-custom');
+  genderCustom.setAttribute('placeholder', 'Gênero');
+  genderBox.appendChild(genderCustom);
+});

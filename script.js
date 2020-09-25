deda26108f09cdcf02b1c18faae995b037aab1b7
Rodolfo <rodolfojr94@gmail.com>
2020-09-25 08:45:19 -0300

    Feito décimo nono item

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ submitBt.addEventListener('click', function () {
   const getUser = document.querySelector('#user-email-phone').value;
   alert(getUser);
 });
+
+let personalGenre = document.querySelector('#personalized');
+/* Podemos adicionar outros elementos na hora do layout */
+personalGenre.addEventListener('change',() => {
+  
+  let perGen = document.createElement('input'),
+      container = document.querySelector('#personalize-container');
+
+  perGen.name = "gender-custom";
+  perGen.placeholder = "Gênero (opcional)";
+  perGen.type = "text";
+  
+  container.appendChild(perGen);
+
+});

117bab2b5f0725eb661e54583fc3d7d06afb99f4
Rodrigo <rd64103@gmail.com>
2020-09-25 16:37:34 -0300

    duvido q passe o requisito 20, capaz dele apenas errar tudo e ir para 0%

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function checkForm() {
   let error = false;
+  let i = 0
   document.querySelectorAll('.cadastro input').forEach((input) => {
-    if (input.type !== 'radio') {
+    if ((input.type !== 'radio') && (input.className !== "personalizado")) {
       if (input.value === '') {
         error = true;
       }
     }
   });
-
-  if (error) {
+  if (error === true) {
     document.querySelector('.error').style.display = 'block';
+  }else {
+    let nome = document.createTextNode(`Olá, ${document.querySelectorAll('.name')[0].value} ${document.querySelectorAll('.name')[1].value}`)
+    let email = document.createTextNode(document.querySelector('.email').value);
+    let genero = document.createTextNode(document.querySelector('input[name="gender"]:checked').value);
+    document.querySelector('.cadastro').style.display = "none"
+    document.querySelector('#um').appendChild(nome)
+    document.querySelector('#dos').appendChild(email)
+    document.querySelector('#tres').appendChild(genero)
   }
 }
 

d7d964106654b705c5fc4215a5fb105fdc828a4c
Galaraz <jvictor.si.2013@gmail.com>
2020-09-25 18:12:46 -0300

    code-climate o retorno3012

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 function validar() {
   // Leitura dos dados
-  const userLogin = document.getElementById("user-email-phone").value;
-  const userPassord = document.getElementById("user-password").value;
+  const userLogin = document.getElementById('user-email-phone').value;
+  const userPassord = document.getElementById('user-password').value;
 
-  let mensagem = "";
+  let mensagem = '';
   // Comparações
-  if (userLogin === "") {
-    mensagem += "Campo nome é obrigatório!!!\n";
+  if (userLogin === '') {
+    mensagem += 'Campo nome é obrigatório!!!\n';
   }
-  if (userPassord === "") {
-    mensagem += "Campo e-mail é obrigatório!!!\n";
-  }
-
-  if (mensagem !== "") {
-    alert("Email ou telefone");
+  if (userPassord === '') {
+    mensagem += 'Campo e-mail é obrigatório!!!\n';
   }
 
-  if (mensagem === "") {
-    alert("login efetuado com sucesso ");
+  if (mensagem !== '') {
+    alert('Email ou telefone');
   }
-  return 
+
+  if (mensagem === '') {
+    alert('login efetuado com sucesso');
+  }
+
 }
 function validarNovoUsuario() {
   let listaForm = {};
-  listaForm.firstName = document.getElementById("firstname").value;
-  listaForm.lastName = document.getElementById("lastname").value;
-  listaForm.email = document.getElementById("phone_email").value;
-  listaForm.password = document.getElementById("password").value;
-  listaForm.genero = document.querySelector("[type=radio]:checked");
-  listaForm.dataNasc = document.getElementById("birthdate").value;
-  if (listaForm !== "") {
+  listaForm.firstName = document.getElementById('firstname').value;
+  listaForm.lastName = document.getElementById('lastname').value;
+  listaForm.email = document.getElementById('phone_email').value;
+  listaForm.password = document.getElementById('password').value;
+  listaForm.genero = document.querySelector('[type=radio]:checked');
+  listaForm.dataNasc = document.getElementById('birthdate').value;
+  if (listaForm !== '') {
     alert("nao tem nada aqui");
   }
   return listaForm;

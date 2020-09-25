9cb3116546bc1a95b591f5a4361e9279d1a99430
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-09-25 18:57:08 -0300

    Requisito 18, Validar se todos os campos foram preenchidos ao clicar no botão Cadastre-se.

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 const buttonLogin = document.querySelector('#button-login');
+const buttonFacebookRegister = document.querySelector('#facebook-register');
 
 buttonLogin.addEventListener('click', function () {
   const campoEmail = document.querySelector('#user-email-phone').value;
   alert(campoEmail);
+});
+
+function verifyFormRegisterInputs() {
+  const formRegisterInputs = document.getElementsByClassName('required');
+  for (let input = 0; input <= formRegisterInputs.length - 1; input += 1) {
+    if (formRegisterInputs[input].value === '') {
+      return false;
+    }
+  }
+  return true;
+}
+
+function verifyRadioGenderChecked() {
+  const formRegisterRadio = document.getElementsByClassName('radio');
+  for (let input = 0; input <= formRegisterRadio.length - 1; input += 1) {
+    if (formRegisterRadio[input].checked) {
+      return true;
+    }
+  }
+  return false;
+}
+
+buttonFacebookRegister.addEventListener('click', (event) => {
+  const errorMessage = document.getElementById('error-message');
+  if (!verifyRadioGenderChecked() || !verifyFormRegisterInputs()) {
+    errorMessage.innerHTML = 'Campos inválidos';
+    errorMessage.style = '';
+    event.preventDefault();
+  }
 });

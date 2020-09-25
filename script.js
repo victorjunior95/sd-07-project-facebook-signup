546e97f8022bc315c9063a989852c68764eaa498
Emanuelle Brasil <emanuelle.rbl@gmail.com>
2020-09-25 12:01:25 -0300

    corrigindo issues codeclimate

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.querySelector('#button-login');
 button.addEventListener('click', function () {
   const userEmail = document.getElementById('user-email-phone').value;
-  alert(userEmail); 
+  alert(userEmail);
 });

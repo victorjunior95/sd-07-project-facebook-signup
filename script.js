2148727a2f303f03b89dda47c02a9f8f252c79e2
Mauro Junqueira <maurojunqueira@pop-os.localdomain>
2020-09-26 08:59:40 -0300

    Incluindo validacao cadastro

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const button = document.getElementById('button-login');
 button.addEventListener('click', function () {
   alert('Email ou telefone');
 });
+
+const buttonRegister = document.querySelector('facebook-register');
+buttonRegister.addEventListener('click', function () {
+  alert('Campos inválidos');
+});

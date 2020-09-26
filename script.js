9929d0ef41d597b0c12766df889c21b569052a89
Filipi Firmino <contatoic3web@gmail.com>
2020-09-26 18:44:57 -0300

    Ajustes no html e implementaçao do alert do login

---
+++ script.js
@@ -0,0 +1,5 @@
+const buttonLogin = document.querySelector('#button-login');
+buttonLogin.addEventListener('click', () => {
+  const login = document.querySelector('#user-email-phone');
+  alert (`${login.value }`)
+})

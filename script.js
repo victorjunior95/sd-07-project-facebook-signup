57a21a89c424ce63ccbd524ceeb9ba097fe36190
Rodolfo <rodolfojr94@gmail.com>
2020-09-26 20:31:49 -0300

    Resposta aos 64x0 do code climate:hammer:

--- script.js
+++ script.js
@@ -2,56 +2,26 @@
 document.querySelector('#button-login').addEventListener('click', () => {
   const getUser = document.querySelector('#user-email-phone').value;
   alert(getUser);
-
 });
 
 document.querySelector('#personalized').addEventListener('change', () => {
-
-  if( document.querySelector('#personalize-container input') === null){
-  const perGen = document.createElement('input');
-  const container = document.querySelector('#personalize-container');
-  perGen.name = 'gender-custom';
-  perGen.placeholder = 'Gênero (opcional)';
-  perGen.type = 'text';
-  perGen.id = 'perGen';
-  container.appendChild(perGen);
-  };
-
-});
-
-document.querySelector('#facebook-register').addEventListener('click', () => {
-
-  removeMsgWarning();
-
-  const formSub = document.querySelectorAll('#subscribe input');
-  let blankFields = false;
-
-  for( let i = 0 ; i < formSub.length ; i++ ) {
-
-    const element = formSub[i];
-    
-    if ( element.value === "" ) {
-
-      blankFields = true;
-
-    }
-    else if (document.querySelector('input[name="genre"]:checked') === null){
-      
-      blankFields = true;
- 
-    };
-
-  };
-
-  if ( blankFields === true ) { createMsgWarning();};
-
+  if(document.querySelector('#personalize-container input') === null) {
+    const perGen = document.createElement('input');
+    const container = document.querySelector('#personalize-container');
+    perGen.name = 'gender-custom';
+    perGen.placeholder = 'Gênero (opcional)';
+    perGen.type = 'text';
+    perGen.id = 'perGen';
+    container.appendChild(perGen);
+  }
 });
 
 removeMsgWarning = () => {
-const container = document.querySelector('#invalidFields');
-const containerP = document.querySelector('#invalidFields p');
-if( containerP !== null ){container.removeChild(containerP)};
+  const container = document.querySelector('#invalidFields');
+  const containerP = document.querySelector('#invalidFields p');
+  if( containerP !== null ){container.removeChild(containerP)};
 }
+
 createMsgWarning = () => {
   if(document.querySelector('#invalidFields p') === null){
   const paragraph = document.createElement('p');
@@ -60,3 +30,19 @@ createMsgWarning = () => {
   document.querySelector('#invalidFields').appendChild(paragraph);
   };
 };
+
+document.querySelector('#facebook-register').addEventListener('click', () => {
+  removeMsgWarning();
+  const formSub = document.querySelectorAll('#subscribe input');
+  let blankFields = false;
+  for (let i = 0; i < formSub.length; i += 1) {
+    const element = formSub[i];
+    if (element.value === "") {
+      blankFields = true;
+    }
+    else if (document.querySelector('input[name="genre"]:checked') === null){
+      blankFields = true;
+    };
+  };
+  if (blankFields === true) { createMsgWarning();};
+});

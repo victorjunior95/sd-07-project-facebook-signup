03308bfc6c2ee9c1e5c797c089f3564458923228
jvitorof <j.vitoroflores@gmail.com>
2020-09-26 17:17:18 -0300

    Req#20

--- script.js
+++ script.js
@@ -81,12 +81,22 @@ window.onload = () => {
 subimitButton.addEventListener('click', function (event) {
   event.preventDefault();
   const adicionaMensagem = document.querySelector('.open-account');
+  const rightContent = document.querySelector('.right-content');
   const valoresInput = document.querySelectorAll('.custom-control-label');
   const mensagem = document.createElement('p');
   adicionaMensagem.appendChild(mensagem);
   for (let index = 0; index < valoresInput.length; index += 1) {
     if (valoresInput[index].value === '') {
       mensagem.innerText = 'Campos inválidos';
+    } else {
+      const genderChecked = document.querySelectorAll('.custom-radio');
+      for (let jindex = 0; jindex < genderChecked.length; jindex += 1) {
+        if (genderChecked[jindex].checked) {
+          rightContent.innerHTML = `Olá, ${valoresInput[0].value} ${valoresInput[1].value}
+          ${valoresInput[2].value}
+          ${valoresInput[4].value} ${genderChecked[jindex].value}`;
+        }
+      }
     }
   }
 });

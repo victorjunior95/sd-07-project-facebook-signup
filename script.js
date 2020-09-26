9688c96c5fb286aeae44fb9ef74c693b0b6665d2
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-26 16:34:24 -0300

    requisito 17 e 18

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const buttonLogin = document.querySelector('#button-login');
 const emailOrPhone = document.querySelector('#user-email-phone');
+const inputs = document.querySelectorAll('input');
+const buttonRegister = document.querySelector('#facebook-register');
+const formRegister = document.querySelector('#form-register');
+
 // alert('funcionou);
 buttonLogin.addEventListener('click', function alerta() {
   alert(`${emailOrPhone.value}`);
 });
+
+function validate() {
+  event.preventDefault();
+  const message = document.createElement('h1');
+  for (let i = 0; i < inputs.length; i += 1) {
+    if (inputs[i].value === '') {
+      message.innerText = 'Campos inválidos';
+    }
+  }
+  formRegister.appendChild(message);
+}
+
+buttonRegister.addEventListener('click', validate);

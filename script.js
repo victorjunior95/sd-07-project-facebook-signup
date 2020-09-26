ca47c7c11bf06263e00fb0f52b0feb524ebb1faf
Lets <letsferreiratx@gmail.com>
2020-09-26 10:27:20 -0300

    criado mensagem de campo invalido

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ buttonLogin.addEventListener('click', function(){
   const email = document.getElementById('user-email-phone').value;
   alert(email);
 })
+
+const buttonSignUp = document.querySelector('#facebook-register');
+buttonSignUp.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const inputs = document.querySelectorAll('.subscription-form input');
+  inputs.forEach(element => {
+
+    console.log(element.value);
+    if (element.value === '' ) {
+      const p = document.querySelector('#invalid-camp');
+      p.textContent = 'Campos inválidos';
+
+    }
+  });
+})

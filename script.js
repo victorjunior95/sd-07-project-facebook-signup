4abe8bec3eb3a160933662a0cacbc9e3c9f0eef2
jvitorof <j.vitoroflores@gmail.com>
2020-09-26 11:50:12 -0300

    juntando Pair Programming

--- script.js
+++ script.js
@@ -4,3 +4,76 @@ const loginInfo = document.querySelector('#user-email-phone');
 loginButton.addEventListener('click', function () {
   alert(loginInfo.value);
 });
+
+const adicionaMensagem = document.querySelector('.open-account');
+
+
+
+
+
+
+
+
+const valida = new JustValidate('.open-account', {
+  rules: {
+    firstname: {
+      required: true,
+    },
+    lastname: {
+      required: true,
+    },
+    phone_email: {
+      required: true,
+    },
+    password: {
+      required: true,
+      strength: {
+        default: true,
+      }    
+    },
+    birthdate: {
+      required: true,
+    },
+    gender: {
+      required: true,
+    },
+  },
+
+  messages: {
+    firstname: {
+      required: 'Campos inválidos',
+    },
+    lastname: {
+      required: 'Campos inválidos',
+    },
+    phone_email: {
+      required: 'Campos inválidos',
+    },
+    password: {
+      required: 'Campos inválidos',
+      strength: {
+        default: 'Senha fraca',
+      }    
+    },
+    birthdate: {
+      required: 'Campos inválidos',
+    },
+    gender: {
+      required: 'Campos inválidos',
+    },
+  },
+
+  colorWrong: 'red',
+
+  invalidFormCallback: function (errors) {
+    let mensagem = document.createElement('p')
+    mensagem.innerHTML = '<strong>Campos inválidos</strong>'
+    adicionaMensagem.appendChild(mensagem)
+    console.log(errors);
+  },
+
+});
+
+window.onload = () => {
+  valida();
+}

6ac43ae49174f9dcea4b913d3415f244934a34d0
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-09-26 18:27:33 -0300

    Fazendo Requisito 20 Parte 5 - 25/09/2020

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function alertLogin() {
-  const buttonForm = document.querySelector("#button-login");
-  const loginInput = document.querySelector("#user-email-phone");
+  const buttonForm = document.querySelector('#button-login');
+  const loginInput = document.querySelector('#user-email-phone');
 
-  buttonForm.addEventListener("click", function () {
+  buttonForm.addEventListener('click', function () {
     alert(loginInput.value);
   });
 }
 
 const newInformationUser = () => {
-  const rightContainer = document.querySelector(".right-content");
-  const name = document.querySelector(".name");
-  const lastname = document.querySelector(".lastname");
-  const phone = document.querySelector(".phoneEmail");
-  const birthdate = document.querySelector(".birthdate");
-  const newMemberContainer = document.createElement("div");
-  const paragraphy = document.createElement("p");
+  const rightContainer = document.querySelector('.right-content');
+  const name = document.querySelector('.name');
+  const lastname = document.querySelector('.lastname');
+  const phone = document.querySelector('.phoneEmail');
+  const birthdate = document.querySelector('.birthdate');
+  const newMemberContainer = document.createElement('div');
+  const paragraphy = document.createElement('p');
 
   let gender = document.querySelector('input[name="gender"]:checked');
-  if (gender.value === "Personalizado") {
+  if (gender.value === 'Personalizado') {
     gender = document.querySelector('input[name="gender-custom"]');
   }
   paragraphy.innerText = `Olá, ${name.value} ${lastname.value}<br>
@@ -31,18 +31,18 @@ const newInformationUser = () => {
 
 function registerAlert() {
   const rightContentInputs = document
-    .querySelector("#sign-up-container")
-    .querySelectorAll("input");
-  const buttonRegister = document.querySelector("#facebook-register");
-  buttonRegister.addEventListener("click", function (event) {
+    .querySelector('#sign-up-container')
+    .querySelectorAll('input');
+  const buttonRegister = document.querySelector('#facebook-register');
+  buttonRegister.addEventListener('click', function (event) {
     event.preventDefault();
     let refimputcheck = 0;
     rightContentInputs.forEach((item) => {
-      const requiredContainer = document.querySelector(".required-alert");
+      const requiredContainer = document.querySelector('.required-alert');
 
-      item.required = "required";
-      if (item.value === "") {
-        requiredContainer.innerText = "Campos inválidos";
+      item.required = 'required';
+      if (item.value === '') {
+        requiredContainer.innerText = 'Campos inválidos';
       } else {
         refimputcheck += 1;
       }
@@ -54,16 +54,16 @@ function registerAlert() {
 }
 
 function createInputGender() {
-  const genderOptions = document.querySelector("#other");
-  const genderContainer = document.querySelector(".gender-container");
+  const genderOptions = document.querySelector('#other');
+  const genderContainer = document.querySelector('.gender-container');
 
-  genderOptions.addEventListener("click", function () {
-    const divOtherGender = document.createElement("div");
-    const inputOtherGender = document.createElement("input");
+  genderOptions.addEventListener('click', function () {
+    const divOtherGender = document.createElement('div');
+    const inputOtherGender = document.createElement('input');
 
-    inputOtherGender.required = "required";
-    inputOtherGender.placeholder = "Gênero (opcional)";
-    inputOtherGender.name = "gender-custom";
+    inputOtherGender.required = 'required';
+    inputOtherGender.placeholder = 'Gênero (opcional)';
+    inputOtherGender.name = 'gender-custom';
 
     if (genderContainer.children.length === 3) {
       genderContainer.removeChild(genderContainer.lastChild);

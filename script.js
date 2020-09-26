88b7092216632b2c3b26077713af52190b946a89
Lucas Ribeiro <martins7ribeiro@gmail.com>
2020-09-26 14:00:12 -0300

    birth-date input fixing, custom gender event

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const emailPhoneInput = document.getElementById('user-email-phone');
 signIn.addEventListener('click', () => {
   alert(emailPhoneInput.value);
 });
+
+
+document.querySelector('#custom-gender').addEventListener('click', () => {
+  const customGender = document.createElement('input');
+  customGender.type = 'text';
+  customGender.name = 'gender-custom';
+  customGender.placeholder = 'Gênero (opcional)';
+  document.querySelector('.all-genders').appendChild(customGender);
+});

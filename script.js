9222b404007e82ee958c792bcdb2a55214aa2c67
TIAGO <tiagoesdrasvieira@gmail.com>
2020-09-26 16:04:47 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ loginBtn.addEventListener('click', function (event) {
   const emailInput = document.querySelector('#user-email-phone').value;
   alert(`${emailInput}`);
 });
+
+const validate = new window.JustValidate('.js-form', {
+  rules: {
+    last_name: {
+      required: true,
+      minLength: 3,
+      maxLength: 15
+    },
+    email_phone: {
+      required: true,
+      maxLength: 50
+    },
+    radio: {
+      required: true
+    },
+    birthdate: {
+      required: true,
+      minLength: 10,
+      maxLength: 10
+    },
+    messages: {
+      name: 'Campos inválidos',
+      last_name: 'Campos inválidos',
+      phone_email: 'Campos inválidos',
+      password: 'Campos inválidos',
+      birthdate: 'Campos inválidos',
+      radio: 'Campos inválidos',
+    },
+  }
+});

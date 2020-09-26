358636c8bfc3f014e0eb0bb1a0341341309ebcd4
Filipi Firmino <contatoic3web@gmail.com>
2020-09-26 18:47:42 -0300

    Ajustes CC no JS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const buttonLogin = document.querySelector('#button-login');
 buttonLogin.addEventListener('click', () => {
   const login = document.querySelector('#user-email-phone');
-  alert (`${login.value }`)
-})
+  alert(`${login.value}`);
+});

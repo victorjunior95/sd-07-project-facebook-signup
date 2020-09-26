95e3492fff978cd3ce9cc068c9eab7767e17fc6e
Renato Silva <renatosdev@gmail.com>
2020-09-26 17:41:04 -0300

    Funcao de campo obrigatorio add

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ const emailInput = document.querySelector('#user-email-phone');
 buttonLogin.addEventListener('click', () => {
   alert(`${emailInput.value}`);
 });
+
+const pAlert = document.querySelector('.alert');
+const buttonSubmit = document.querySelector('#facebook-register');
+
+buttonSubmit.addEventListener('click', () => {
+  const inputs = document.querySelectorAll('input');
+  const genders = document.querySelector('.genders');
+  const gendersInputs = genders.querySelectorAll('input');
+
+  inputs.forEach((element) => {
+    if (element.required === true) {
+      if (element.type !== 'radio') {
+        if (element.value === '') {
+          pAlert.innerText = 'Campos inválidos';
+        }
+      }
+    }
+  });
+
+  let count = 0;
+
+  gendersInputs.forEach((element) => {
+    if (!element.checked) {
+      count += 1;
+    }
+    if (count >= 3) {
+      pAlert.innerText = 'Campos inválidos';
+    }
+  });
+});

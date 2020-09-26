59b07c512467a016419ea9544ac6f00070d1d407
Filipi Firmino <contatoic3web@gmail.com>
2020-09-26 19:50:57 -0300

    ajustes CC

--- script.js
+++ script.js
@@ -5,26 +5,25 @@ buttonLogin.addEventListener('click', () => {
 });
 
 const inputGender = document.querySelector('#gender-custom');
-const radioGender = document.querySelector ('#other');
+const radioGender = document.querySelector('#other');
 
 radioGender.addEventListener('change', () => {
-    console.log('chamou');
   if (inputGender.style.display === '' || inputGender.style.display === 'none') {
-      inputGender.style.display = "block";
+   inputGender.style.display = 'block';
   } else {
-    inputGender.style.display = "none";
+   inputGender.style.display = 'none';
   }
-})
+});
 
 
 function checkRadio() {
-    if (inputGender.style.display === 'block') {
-        inputGender.style.display = "none";
-    }
+  if (inputGender.style.display === 'block') {
+   inputGender.style.display = 'none';
+  }
 }
 
 const male = document.querySelector('#male');
-male.addEventListener('change', checkRadio) ;
+male.addEventListener('change', checkRadio);
 
 const female = document.querySelector('#female');
 female.addEventListener('change', checkRadio);

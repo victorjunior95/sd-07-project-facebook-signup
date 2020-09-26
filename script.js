ec34d7801a56ae92da32c82d0c014d0ce2879bab
Ricardo Augusto <ricardo.rals.dev@gmail.com>
2020-09-26 10:03:11 -0300

    ajuste

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 // Colocar/Retirar a caixa de texto de gênero-personalizado;
 const personalizado = document.getElementById('personalizado');
-const select = document.getElementById('genero-personalizado');
-const masculino = document.getElementById('masculino');
-const feminino = document.getElementById('feminino');
-
+// const select = document.getElementById('genero-personalizado');
+// const masculino = document.getElementById('masculino');
+// const feminino = document.getElementById('feminino');
+const genero = document.getElementById('gender');
 personalizado.addEventListener('change', function () {
+  const select = document.createElement('input');
   select.style.display = 'block';
-  // document.getElementById('gender-custom').innerText = '';
-  // const select = document.createElement('input');
-  // select.style.display = 'block';
-  // select.placeholder = 'Gênero (opcional)';
-  // select.name = 'gender-custom';
-  // select.id = 'gender-custom';
-  // select.type = 'text';
-  // genero.appendChild(select);
+  select.placeholder = 'Gênero (opcional)';
+  select.name = 'gender-custom';
+  select.id = 'gender-custom';
+  select.type = 'text';
+  genero.appendChild(select);
 });
-masculino.addEventListener('change', function () {
-  select.style.display = 'none';
-});
-feminino.addEventListener('change', function () {
-  select.style.display = 'none';
-});
+// masculino.addEventListener('change', function () {
+//     if(document.getElementById('gender-custom'){
+//   document.getElementById('gender-custom').innerHTML = '';
+//     }
+// });
+// feminino.addEventListener('change', function () {
+//     if(document.getElementById('gender-custom'){
+//     document.getElementById('gender-custom').innerHTML = '';
+//     }
+// });
 
 // Alerta do botão login
 const botaoLogin = document.getElementById('button-login');

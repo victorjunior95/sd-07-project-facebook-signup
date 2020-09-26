a4e66955c78c8e4be382627adc15f540b0274d1a
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-09-26 11:42:47 -0300

    requisito 19

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const loginButton = document.querySelector('#button-login');
 const loginInfo = document.querySelector('#user-email-phone');
+const genderCustom = document.querySelector('#Personalizado');
 
 loginButton.addEventListener('click', function () {
   alert(loginInfo.value);
+});
+
+genderCustom.addEventListener('click', () => {
+  const customInput = document.createElement('input');
+  customInput.setAttribute('name', 'gender-custom');
+  customInput.placeholder = 'Gênero (opcional)';
+  document.querySelector('#custom').appendChild(customInput);
 });

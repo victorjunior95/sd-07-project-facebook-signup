729ddea8ee81393a7816c748aca9e0d3b2b1e1c5
Filipi Firmino <contatoic3web@gmail.com>
2020-09-26 19:45:08 -0300

    Implementado o input para o genero personalizado

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ buttonLogin.addEventListener('click', () => {
   const login = document.querySelector('#user-email-phone');
   alert(`${login.value}`);
 });
+
+const inputGender = document.querySelector('#gender-custom');
+const radioGender = document.querySelector ('#other');
+
+radioGender.addEventListener('change', () => {
+    console.log('chamou');
+  if (inputGender.style.display === '' || inputGender.style.display === 'none') {
+      inputGender.style.display = "block";
+  } else {
+    inputGender.style.display = "none";
+  }
+})
+
+
+function checkRadio() {
+    if (inputGender.style.display === 'block') {
+        inputGender.style.display = "none";
+    }
+}
+
+const male = document.querySelector('#male');
+male.addEventListener('change', checkRadio) ;
+
+const female = document.querySelector('#female');
+female.addEventListener('change', checkRadio);

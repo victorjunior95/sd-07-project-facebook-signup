ff93465d86536c3d4c726bd968a2cec0a78056e5
Galaraz <jvictor.si.2013@gmail.com>
2020-09-25 21:51:03 -0300

    code-climate the return 2

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
-function validar() {
-  // Leitura dos dados
-  const userLogin = document.getElementById('user-email-phone').value;
-  const userPassord = document.getElementById('user-password').value;
-
-  let mensagem = '';
-  // Comparações
-  if (userLogin === '') {
-    mensagem += 'Campo nome é obrigatório!!!\n';
-  }
-  if (userPassord === '') {
-    mensagem += 'Campo e-mail é obrigatório!!!\n';
-  }
-
-  if (mensagem !== '') {
-    alert('"Email ou telefone"');
-  }
-
-  if (mensagem === '') {
-    alert('login efetuado com sucesso');
-  }
-}
-validar();
-function validarNovoUsuario() {
-  const listaForm = {};
-  listaForm.firstName = document.getElementById('firstname').value;
-  listaForm.lastName = document.getElementById('lastname').value;
-  listaForm.email = document.getElementById('phone_email').value;
-  listaForm.password = document.getElementById('password').value;
-  listaForm.genero = document.querySelector('[type=radio]:checked');
-  listaForm.dataNasc = document.getElementById('birthdate').value;
-  if (listaForm !== '') {
-    alert('Campos inválidos');
-  }
-  return listaForm;
-}
-validarNovoUsuario();
+const button = document.getElementById('button-login');
+button.addEventListener('click', function () {
+  alert('Email ou telefone');
+});

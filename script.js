1e99b9e00e93c83a72ce371bf370078bcdc176bd
Lets <letsferreiratx@gmail.com>
2020-09-26 10:57:33 -0300

    criado gender-custom

--- script.js
+++ script.js
@@ -10,12 +10,20 @@ buttonSignUp.addEventListener('click', (event) => {
 
   const inputs = document.querySelectorAll('.subscription-form input');
   inputs.forEach(element => {
-
-    console.log(element.value);
     if (element.value === '' ) {
       const p = document.querySelector('#invalid-camp');
       p.textContent = 'Campos inválidos';
 
     }
   });
+})
+
+const inputCustom = document.querySelector('#Personalizado');
+inputCustom.addEventListener('click', () => {
+  const genderCustom = document.createElement('input');
+  genderCustom.name = 'gender-custom';
+  genderCustom.placeholder = 'Gênero (opcional)';
+  genderCustom.type = 'text';
+  document.getElementById('label-gender').appendChild(genderCustom);
+
 })

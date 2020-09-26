0990668dd6619318d63af3e93b0b37c3ee9aa1b6
Mauro Junqueira <maurojunqueira@pop-os.localdomain>
2020-09-26 09:58:49 -0300

    exclusao do event do cadastro

--- script.js
+++ script.js
@@ -3,8 +3,4 @@ button.addEventListener('click', function () {
   alert('Email ou telefone');
 });
 
-const buttonRegister = document.querySelector('facebook-register');
-buttonRegister.addEventListener('click', function () {
-  alert('Campos inválidos');
-  //  alert do registro
-});
+

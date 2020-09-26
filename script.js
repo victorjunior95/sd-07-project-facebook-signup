35ede7f0baa9a93acf1883a99c67fbd155a96cb4
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-09-26 17:14:07 -0300

    requisito 20

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+function newContentRight(data) {
+  console.log(data)
+  if (data.length >= 4) {
+    const parentRightContent = document.querySelectorAll('.main-content')[0];
+    const divOldRightContent = document.querySelector('#right-content');
+    parentRightContent.removeChild(divOldRightContent);
+    const newDivRight = document.createElement('div');
+    newDivRight.className = 'new-content-right';
+    parentRightContent.appendChild(newDivRight);
+    const ulApresentation = document.createElement('ul');
+    newDivRight.appendChild(ulApresentation);
+    const liItem = document.createElement('li');
+    liItem.className = 'welcome-mensage';
+    liItem.innerText = `Olá, ${data[0]} ${data[1]}.
+    E-mail ou telefone: ${data[2]}
+    Data de nascimento: ${data[3]}
+    Gênero: ${data[4]}`;
+    ulApresentation.appendChild(liItem);
+  }
+};
+
 const botaoLogin = document.getElementById('button-login');
 botaoLogin.addEventListener('click', function () {
   const emailTelefone = document.querySelector('#user-email-phone').value;
@@ -11,14 +32,30 @@ customButton.addEventListener('change', function () {
   divgender.style.visibility = 'visible';
 });
 
+let valuesCaptured = [];
 const registerButton = document.querySelector('#facebook-register');
 registerButton.addEventListener('click', function () {
   const inputsFormRegister = document.querySelectorAll('.validation');
   const validationMsg = document.querySelector('#validation-msg');
+
   for (let index = 0; index < inputsFormRegister.length; index += 1) {
     if (inputsFormRegister[index].value === '') {
       validationMsg.innerText = 'Campos inválidos';
       validationMsg.style.color = 'red';
+      return
+    }
+    if (inputsFormRegister[index].name !== 'password') {
+      valuesCaptured.push(inputsFormRegister[index].value);
     }
   }
+    const radioOptions = document.querySelectorAll('input[name=gender]');
+    let selectedRadioOption;
+    for (let index = 0; index < radioOptions.length; index += 1) {
+      if (radioOptions[index].checked) {
+        selectedRadioOption = radioOptions[index].value;
+        valuesCaptured.push(selectedRadioOption);
+      }
+    }
+  newContentRight(valuesCaptured);
+  valuesCaptured = [];
 });

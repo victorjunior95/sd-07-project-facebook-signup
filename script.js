8a816eb490e5dd6ab6bc611f5e09b71099a58936
josielvs <josielvs@gmail.com>
2020-09-26 16:17:12 -0300

    Preparando os dados para o requisito 20

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+function newContentRight(data) {
+  if (data.length >= 4) {
+    const parentRightContent = document.querySelectorAll('.main-content')[0];
+    const divOldRightContent  = document.querySelector('#right-content');
+    parentRightContent.removeChild(divOldRightContent);
+    const newDivRight = document.createElement('div');
+    newDivRight.className = 'new-content-right';
+    parentRightContent.appendChild(newDivRight);
+    const ulApresentation = document.createElement('ul');
+    newDivRight.appendChild(ulApresentation);
+    const liItem = document.createElement('li');
+    liItem.className = 'welcome-mensage';
+    liItem.innerText = `Olá, ${data[0]} ${data[1]}.
+    E-mail ou telefone: ${data[2]}
+    Data de nascimento: ${data[3]}
+    Gênero: ${data[4]}`;
+    ulApresentation.appendChild(liItem);
+  }
+}
+
 const botaoLogin = document.getElementById('button-login');
 botaoLogin.addEventListener('click', function () {
   const emailTelefone = document.querySelector('#user-email-phone').value;
@@ -15,10 +35,19 @@ const registerButton = document.querySelector('#facebook-register');
 registerButton.addEventListener('click', function () {
   const inputsFormRegister = document.querySelectorAll('.validation');
   const validationMsg = document.querySelector('#validation-msg');
+  let valuesCaptured = []
   for (let index = 0; index < inputsFormRegister.length; index += 1) {
+    /* 
     if (inputsFormRegister[index].value === '') {
       validationMsg.innerText = 'Campos inválidos';
       validationMsg.style.color = 'red';
+    } else {
+      if (inputsFormRegister[index].name !== 'password') {
+       valuesCaptured.push(inputsFormRegister[index].value);
+      }
     }
+    */
+  // console.log(inputsFormRegister[index])
   }
+  // newContentRight(valuesCaptured);
 });

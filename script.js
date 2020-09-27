39d09d235989d770e4eefc51ab0d31531d0e7c86
Leonardo <lecastro16@gmail.com>
2020-09-27 14:24:27 -0300

    Ajustando Code Climate V3

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
 const buttonlogin = document.getElementById('button-login');
 buttonlogin.addEventListener('click', () => {
-    const emailPhone = document.forms.myForm.fnomeEmail.value;
-    const passwd = document.forms.myForm.fpassword.value;
-    if (emailPhone !== '' || emailPhone !== null) {
-        if (passwd === '' || passwd === null) {
-            return false;
-        } else {
-            alert(`${emailPhone}`);
-        }
+  const emailPhone = document.forms.myForm.fnomeEmail.value;
+  const passwd = document.forms.myForm.fpassword.value;
+  if (emailPhone !== '' || emailPhone !== null) {
+    if (passwd === '' || passwd === null) {
+        return false;
+    } else {
+        alert(`${emailPhone}`);
     }
-    return emailPhone;
+  }
+  return emailPhone;
 });
 
 const genderP = document.querySelector('#gender-p');
 genderP.addEventListener('change', () => {
-const genderOptions = document.querySelector('.gender-options');
-const inputGender = document.createElement('input');
-inputGender.name = 'gender-custom';
-inputGender.placeholder = 'Gênero (opcional)';
-inputGender.type = 'text';
-genderOptions.appendChild(inputGender);
+  const genderOptions = document.querySelector('.gender-options');
+  const inputGender = document.createElement('input');
+  inputGender.name = 'gender-custom';
+  inputGender.placeholder = 'Gênero (opcional)';
+  inputGender.type = 'text';
+  genderOptions.appendChild(inputGender);
 });
 
+let count = 0;/* referência projeto higoramorim-facebook-project*/
 function addMsgError(check) {
 const registrationForm = document.querySelector('.registration-form');
-    if (check) {
-        const p = document.createElement('p');
-        count += 1;
-        if (count <= 1) {
-            p.innerHTML = 'Campos inválidos';
-            registrationForm.appendChild(p);
-        }
+  if (check) {
+    const p = document.createElement('p');
+    count += 1;
+    if (count <= 1) {
+        p.innerHTML = 'Campos inválidos';
+        registrationForm.appendChild(p);
     }
-}
+  }
+};
 
-let count = 0;/* referência projeto higoramorim-facebook-project*/
 const buttonFacebookRegister = document.querySelector('#facebook-register');
 buttonFacebookRegister.addEventListener('click', () => {
-    const input = document.querySelectorAll('input')
-    let check = false;
-    input.forEach(item => {
-        if (item.id !== 'user-email-phone' && item.id !== 'user-password') {
-            if (item.value === '' || item.value === null) {
-                check = true;
-            }
-        }
-    });
-    addMsgError(check);
+  const input = document.querySelectorAll('input');
+  let check = false;
+  input.forEach((item) => {
+      if (item.id !== 'user-email-phone' && item.id !== 'user-password') {
+          if (item.value === '' || item.value === null) {
+              check = true;
+          }
+      }
+  });
+  addMsgError(check);
 });

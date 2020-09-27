c26f93fd603151466d473bf253ff6ac21e554990
Anderson Santana <anderslash@gmail.com>
2020-09-27 14:16:10 -0300

    Correcao na mensagem de campo invalido

--- script.js
+++ script.js
@@ -37,6 +37,10 @@ Array.prototype.forEach.call(radios, function (radio) {
 });
 
 function validateForm() {
+  const warning = document.getElementById('warning');
+  if (warning != null) {
+    warning.parentElement.removeChild(warning);
+  }
   const fields = [
     document.forms['registration-form'].firstname,
     document.forms['registration-form'].lastname,
@@ -47,7 +51,13 @@ function validateForm() {
   ];
   for (let index = 0; index < fields.length; index += 1) {
     if (fields[index].value === '') {
-      fields[index].value = 'Campos inválidos';
+      // fields[index].value = 'Campos inválidos';
+      const registrationForm = document.forms['registration-form'];
+      const warning = document.createElement('div');
+      warning.setAttribute('id', 'warning');
+      warning.innerText = 'Campos inválidos';
+      registrationForm.appendChild(warning);
+      break;
     }
   }
 }

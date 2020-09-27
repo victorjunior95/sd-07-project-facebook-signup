d1030b863189b3d0a7dfa60686d4c06a9208e83d
alexandrefaustino <alexandrefauldade@gmail.com>
2020-09-27 09:58:02 -0300

    Commit: 0.2.5

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.getElementById('button-login').addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
 });
+
+
+new JustValidate('.js-form', {
+  rules: {
+    text: {
+      required: true,
+    },
+  },
+  messages: {
+    text: {
+      required:  'Campos inválidos',
+    },
+  },
+});

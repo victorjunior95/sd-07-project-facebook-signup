24f739b0e415218604d19b615e7499bc4ac141d3
Jorge Alves <alvesdjorge@gmail.com>
2020-09-27 12:56:56 -0300

    add func valida formulario

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
-function alertButton() {
-  const emailTelefone = document.getElementById('user-email-phone').value;
-  if (emailTelefone !== '') {
-    alert(emailTelefone);
+const pMensageInvalid = document.getElementById('mensage-invalid');
+const btButtonLogin = document.getElementById('button-login');
+btButtonLogin.addEventListener('click', function () {
+  const inpUserEmailPhone = document.getElementById('user-email-phone');
+  alert(inpUserEmailPhone.value);
+});
+
+
+const btnSubmit = document.getElementById('facebook-register');
+btnSubmit.addEventListener('click', function (event) {
+  event.preventDefault();
+  const inpFirstname = document.getElementsByName('firstname')[0];
+  const inpLastname = document.getElementsByName('lastname')[0];
+  const inpPhoneEmail = document.getElementsByName('phone_email')[0];
+  const inpPassword = document.getElementsByName('password')[0];
+  const inpBirthdate = document.getElementsByName('birthdate')[0];
+  const inpGender = document.getElementsByName('gender');
+  let innerIf = false;
+  if (inpFirstname.value.trim() === '') {
+    innerIf = true;
   }
-}
-document.getElementById('button-login').addEventListener('click', alertButton);
+  if (inpLastname.value.trim() === '') {
+    innerIf = true;
+  }
+  if (inpPhoneEmail.value.trim() === '') {
+    innerIf = true;
+  }
+  if (inpPassword.value.trim() === '') {
+    innerIf = true;
+  }
+  if (inpBirthdate.value.trim() === '') {
+    innerIf = true;
+  }
+  if (!(inpGender[0].checked) && !(inpGender[1].checked) && !(inpGender[2].checked)) {
+    innerIf = true;
+  }
+
+  if (innerIf) {
+    pMensageInvalid.innerText = 'Campos inválidos';
+  }
+  alert('Campos inválidos');
+});
+
+
+// function validaRadio() {
+//   const otherPers = document.getElementById('other').value
+//   if (otherPers !== '' ) {
+//     alert(otherPers);
+//   }
+// }

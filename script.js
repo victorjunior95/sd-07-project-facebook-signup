0f5fa9e724e0b9658d26063f5f15295bba89f811
barrrosomarcio <barrrosomarcio@gmail.com>
2020-09-27 12:47:43 -0300

    alterações

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const alertText = document.querySelector('#user-email-phone');
 const alertButton = document.querySelector('#button-login');
 
-function sendAlert() {
-  alert(alertText);
-}
-
-alertButton.addEventListener('click', sendAlert());
+alertButton.addEventListener('click', function (){
+  alert(alertText.nodeValue);
+});

b10f059c51b6658e6c1d8fb2692e6e8864c31629
lisvalver <lisvalver.atte@gmail.com>
2020-09-27 14:53:46 -0300

    requisitos 18 e 20

--- script.js
+++ script.js
@@ -11,25 +11,29 @@ radioPerso.addEventListener('click', () => {
   inputPerso.innerHTML = '<input type="text" name="gender-custom" placeholder="Gênero (opcional)">';
 });
 
+
 const btRegister = document.querySelector('#facebook-register');
-const formInputs = document.querySelectorAll('.right-content input');
-const formRadios = document.getElementsByName('gender');
-const formGroup = document.querySelector('.right-content');
+const formGroup = document.querySelector('#form-group');
 btRegister.addEventListener('click', (event) => {
   event.preventDefault();
-  for (let index = 0; index < formInputs.length; index += 1) {
-    if (formInputs[index].value === '') {
-      const invalidFields = document.createElement('div');
-      formGroup.appendChild(invalidFields);
+  const invalidFields = document.createElement('div');
+  formGroup.appendChild(invalidFields);
+  const formGroupInputs = [
+    formGroup.firstname.value,
+    formGroup.lastname.value,
+    formGroup.phone_email.value,
+    formGroup.birthdate.value,
+    formGroup.password.value,
+    formGroup.gender.value,
+  ];
+  for (let index = 0; index < formGroupInputs.length; index += 1) {
+    if (formGroupInputs[index] === '') {
       invalidFields.innerText = 'Campos inválidos';
       break;
+    } else {
+      const checkedFields = `Olá, ${formGroup.firstname.value} ${formGroup.lastname.value}
+      ${formGroup.phone_email.value} ${formGroup.birthdate.value} ${formGroup.gender.value}`;
+      invalidFields.innerHTML = `${checkedFields}`;
     }
-  }
-  if (formRadios[0].checked === false &&
-    formRadios[1].checked === false &&
-    formRadios[2].checked === false) {
-    const invalidFields = document.createElement('div');
-    formGroup.appendChild(invalidFields);
-    invalidFields.innerText = 'Campos inválidos';
   }
 });

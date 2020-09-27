8cd05ca3f3aa0e35d17df7afe6031bc0a9039ffa
Anderson Santana <anderslash@gmail.com>
2020-09-27 10:21:54 -0300

    Desenvolvendo logica do botao Cadastre-se

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const button = document.getElementById('button-login');
-button.addEventListener('click', function () {
+const buttonLogin = document.getElementById('button-login');
+buttonLogin.addEventListener('click', function () {
   const user = document.getElementById('user-email-phone').value;
   alert(user);
+});
+
+const buttonRegister = document.getElementById('facebook-register');
+buttonRegister.addEventListener('click', function () {
+  const formClass = document.getElementById('registration-form');
+  console.log(formClass.children);
 });

bb6077c3cd1081f6d6602f12c17721219a2da003
Fernando Antunes <fernando.antunes1@gmail.com>
2020-09-27 17:30:15 -0300

    Implementado a funcionalidade do campo Genero Personlaizado

--- script.js
+++ script.js
@@ -2,8 +2,26 @@ const btnLogin = document.getElementById('button-login');
 
 const userEmailPhone = document.getElementById('user-email-phone');
 
+const selectGenderCustom = document.getElementById('Personalizado');
+
+const selectGenderFeminine = document.getElementById('Feminino');
+
+const selectGenderMale = document.getElementById('Masculino');
+
 function alertLogin() {
   alert(userEmailPhone.value);
 }
 
+function customHides() {
+  document.getElementById('gender-custom').hidden = true;
+}
+
 btnLogin.addEventListener('click', alertLogin);
+
+selectGenderCustom.addEventListener('click', function () {
+  document.getElementById('gender-custom').hidden = false;
+});
+
+selectGenderFeminine.addEventListener('click', customHides);
+
+selectGenderMale.addEventListener('click', customHides);

75bab5a2853df1abbf8403921c1ade49a625f785
Leonardo <lecastro16@gmail.com>
2020-09-27 19:01:38 -0300

    ajustando code climate x3

--- script.js
+++ script.js
@@ -21,26 +21,28 @@ genderP.addEventListener('change', () => {
   genderOptions.appendChild(inputGender);
 });
 
+let count = 0;/*referência projeto higoramorim-facebook-project*/
 function addMsgError(check) {
   const registrationForm = document.querySelector('.registration-form');
   if (check) {
-      count += 1;
+    const p = document.createElement('p');
+    count += 1;
     if (count <= 1) {
       p.innerHTML = 'Campos inválidos';
       registrationForm.appendChild(p); 
     }
   }
-};
+}
 
 function cleanRightContentDiv() {
   const rightContentDiv = document.querySelector('.right-content');
   rightContentDiv.innerHTML = '';
-};
+}
 
 function returnMsg (nome, email, dataNascimento, gender) {
-  const rightContent = document.querySelector(".right-content");
-  let p = document.createElement('p');
-  p.innerHTML = `Olá ${nome} ${email} ${dataNascimento} ${gender}`
+  const rightContent = document.querySelector('.right-content');
+  const p = document.createElement('p');
+  p.innerHTML = `Olá ${nome} ${email} ${dataNascimento} ${gender}`;
   rightContent.appendChild(p);
 }
 
@@ -48,13 +50,13 @@ let nome;
 let email;
 let dataNascimento;
 let gender;
-let count = 0;/*referência projeto higoramorim-facebook-project*/
-const buttonFacebookRegister = document.querySelector("#facebook-register");
+
+const buttonFacebookRegister = document.querySelector('#facebook-register');
 buttonFacebookRegister.addEventListener('click', () => {
-  let form = document.forms.myForm1;
+  const form = document.forms.myForm1;
   const input = document.querySelectorAll('input')
   let check = false;
-  input.forEach(item => {
+  input.forEach((item) => {
     if (item.id !== 'user-email-phone' && item.id !== 'user-password') {
       if (item.value === '' || item.value === null) {
         check = true;

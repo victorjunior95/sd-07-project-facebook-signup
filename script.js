3a79ee50a99c768e1d81cbee5716585e1b36d708
Felipe Nascimento <flpnascto@gmail.com>
2020-09-27 19:16:01 -0300

    Adicionando requisito 18.

--- script.js
+++ script.js
@@ -2,3 +2,64 @@
 document.getElementById('button-login').addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
 });
+
+// function validarForm() {
+
+// }
+
+const justValidate = new window.JustValidate('.js-form', {
+  rules: {
+    firstname: {
+      required: true,
+    },
+    lastname: {
+      required: true,
+    },
+    email: {
+      required: true,
+      email: true,
+    },
+    password: {
+      required: true,
+      strength: {
+        default: true,
+      },
+    },
+    birthdate: {
+      required: true,
+    },
+    gender: {
+      required: true,
+    },
+  },
+  messages: {
+    firstname: {
+      required: 'Campos inválidos',
+    },
+    lastname: {
+      required: 'Campo obrigatório',
+    },
+    email: {
+      required: 'Campo obrigatório',
+      email: 'E-mail inválido',
+    },
+    password: {
+      required: 'Campo obrigatório',
+      strength: 'Senha deve ter pelo menos uma letra maiúscula, uma minúscula, e um número',
+    },
+    birthdate: {
+      required: 'Campo obrigatório',
+    },
+    gender: {
+      required: 'Campo obrigatório',
+    },
+  },
+
+  // submitHandler: function () {
+  //   validarForm();
+  // },
+});
+
+window.onload = function () {
+  return justValidate;
+};

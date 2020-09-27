2299f846b772b6f7565c726c25e231e3977cf603
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-09-27 12:05:42 -0300

    Commit: 0.3.3

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const divRadios = document.querySelector('.radios-container');
+const name = document.getElementById('name');
+// const lastname = document.getElementById('lastname');
+// const phoneEmail = document.getElementById('phoneEmail');
 
 document.getElementById('button-login').addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
 });
 
+document.getElementById('facebook-register').addEventListener('click', function () {
+  alert(name.value);
+});
 
 const validate = new window.JustValidate('.name', {
   rules: {

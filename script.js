8adce01c86b9e71a2f9810b7d42098acc825ae6c
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-09-27 18:29:42 -0300

    Criaçao funçao genero personalizado

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const button = document.querySelector('#button-login');
 const emailInput = document.querySelector('#user-email-phone');
+const custom = document.querySelector('#personalizado');
+const genderOptions = document.querySelector('.gender-options');
 
 button.addEventListener('click', function () {
   const inputValue = emailInput.value;
   alert(inputValue);
+});
+
+custom.addEventListener('focus', function (target) {
+  console.log(genderOptions.childElementCount);
+
+  if (genderOptions.childElementCount <= 8) {
+    const customInput = document.createElement('input');
+    customInput.type = 'text';
+    customInput.placeholder = 'Gênero(opcional)';
+    genderOptions.appendChild(customInput);
+  }
+  console.log(genderOptions.childElementCount);
 });

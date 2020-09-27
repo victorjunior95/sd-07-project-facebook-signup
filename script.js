df6ec18f93a96fba28527588feb278d3d4d3fca9
Julio Kauer <juliokauer@gmail.com>
2020-09-26 21:01:00 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const signupInputs = document.querySelectorAll('#signup-form input');
 const signupForm = document.querySelector('#signup-form');
 const registerButton = document.querySelector('#facebook-register');
 const customRadio = document.querySelector('#custom');
+const femaleRadio = document.querySelector('#female');
+const maleRadio = document.querySelector('#male');
 const genderContainer = document.querySelector('.gender-container');
 
 buttonLogin.addEventListener('click', function (event) {
@@ -11,8 +13,24 @@ buttonLogin.addEventListener('click', function (event) {
   alert(emailOrTel.value);
 });
 
-registerButton.addEventListener('click', function () {
+function chooseGender() {
+  let gender = 'Personalizado';
+  if (femaleRadio.checked === true) {
+    gender = 'Feminino';
+  } else if (maleRadio.checked === true) {
+    gender = 'Masculino';
+  }
+  return gender;
+}
+
+function welcome() {
+  const rightContent = document.querySelector('.right-content');
+  rightContent.innerHTML = `Olá, ${signupInputs[0].value} ${signupInputs[1].value} <br>${signupInputs[2].value} <br>${signupInputs[4].value}<br>${chooseGender()}`;
+}
+
+registerButton.addEventListener('click', function (event) {
   let valid = true;
+  event.preventDefault();
   signupInputs.forEach((element) => {
     if (element.value === '') {
       valid = false;
@@ -20,6 +38,8 @@ registerButton.addEventListener('click', function () {
   });
   if (valid === false) {
     signupForm.innerHTML = 'Campos inválidos';
+  } else {
+    welcome();
   }
 });
 

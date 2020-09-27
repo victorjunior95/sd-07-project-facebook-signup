0496df9be0a28d4a81a87616cf2862dab927107e
Pablo Pessanha <pablopessanh@gmail.com>
2020-09-26 21:16:04 -0300

    Javascript refatorado para implementacoes de req20

--- script.js
+++ script.js
@@ -1,36 +1,57 @@
+const buttonSubmit = document.querySelector('#facebook-register');
 const buttonLogin = document.getElementById('button-login');
+const inputs = document.querySelectorAll('input');
 const emailInput = document.querySelector('#user-email-phone');
-const genders = document.querySelector('.genders');
-const gendersInputs = genders.querySelectorAll('input');
+const pAlert = document.querySelector('.alert');
+let genderValue = '';
 
 buttonLogin.addEventListener('click', () => {
   alert(`${emailInput.value}`);
 });
 
-const pAlert = document.querySelector('.alert');
-const buttonSubmit = document.querySelector('#facebook-register');
-
-buttonSubmit.addEventListener('click', () => {
-  const inputs = document.querySelectorAll('input');
-
-  inputs.forEach((element) => {
-    if (element.required === true) {
-      if (element.type !== 'radio') {
-        if (element.value === '') {
-          pAlert.innerText = 'Campos inválidos';
-        }
+function checkInputs() {
+  for (let i in inputs) {
+    if (inputs[i].required && inputs[i].type !== 'radio') {
+      if (inputs[i].value === '') {
+        pAlert.innerText = 'Campos inválidos';
+        return false;
       }
     }
-  });
+  }
+  return true;
+}
 
+function checkGender() {
+  const gendersInputs = document.querySelector('.genders').querySelectorAll('input');
   let count = 0;
 
-  gendersInputs.forEach((element) => {
-    if (!element.checked) {
+  gendersInputs.forEach((gender) => {
+    if (!gender.checked) {
       count += 1;
-    }
-    if (count >= 3) {
-      pAlert.innerText = 'Campos inválidos';
+    } else {
+      genderValue = gender.value;
     }
   });
+
+  if (count < 3) {
+    return true;
+  } else {
+    pAlert.innerText = 'Campos inválidos';
+    return false;
+  }
+}
+
+buttonSubmit.addEventListener('click', (e) => {
+  e.preventDefault();
+  const rightContent = document.querySelector('.right-content');
+
+  checkInputs();
+  checkGender();
+
+  if (checkGender() && checkInputs()) {
+    rightContent.innerHTML = `<h1>Olá, ${inputs[2].value} ${inputs[3].value}</h1>
+    <p>Email: ${inputs[4].value}</p>
+    <p>Aniversario: ${inputs[6].value}</p>
+    <p>Genero: ${genderValue}</p>`;
+  }
 });

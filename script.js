c322d81a964f3d90aa4e0769ef0a6132af7d454f
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-09-27 12:33:26 -0300

    styles improvement

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const entrar = document.getElementById('button-login');
 const emailPhone = document.getElementById('user-email-phone');
-const inputs = document.querySelectorAll('.forms input');
 const register = document.getElementById('facebook-register');
 const invalidValues = document.getElementById('invalid');
 const ifChecked = document.getElementById('genero-personalizado');
@@ -10,8 +9,9 @@ entrar.addEventListener('click', () => {
 });
 
 function validateInputs() {
+  const inputs = document.querySelectorAll('.forms input');
   for (let i = 0; i < inputs.length; i += 1) {
-    if (!inputs[i].value) {
+    if (inputs[i].value === '' || inputs[i].checked === false) {
       invalidValues.innerText = 'Campos inválidos';
     }
   }
@@ -23,6 +23,7 @@ ifChecked.addEventListener('click', () => {
     const newInput = document.createElement('input');
     newInput.type = 'text';
     newInput.name = 'gender-custom';
+    newInput.placeholder = 'Gênero (opcional)';
     invalidValues.appendChild(newInput);
   }
 });

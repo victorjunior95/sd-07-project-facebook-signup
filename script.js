fa1ddc7cda000fec4822ce6db085840884fa7aa0
Bruno <bruno_grb@hotmail.com>
2020-09-28 09:43:45 -0300

    iniciando funcao para substituir a seçao right-content

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const signIn = document.getElementById('button-login');
 const emailPhoneInput = document.getElementById('user-email-phone');
+const rightContent = document.querySelector('.right-content');
+const register = document.getElementById('facebook-register');
 
 signIn.addEventListener('click', () => {
   alert(emailPhoneInput.value);
@@ -13,3 +15,15 @@ document.querySelector('#custom-gender').addEventListener('click', () => {
   customGender.placeholder = 'Gênero (opcional)';
   document.querySelector('.all-genders').appendChild(customGender);
 });
+
+function changeRightContent () {
+  const name = document.getElementById('first-name').value;
+  const lastName = document.getElementById('last-name').value;
+  const emailOrPhone = document.getElementById('phone-email').value;
+  const birthdate = document.getElementById('birthdate').value;
+  const gender = document.querySelector('input[type="radio"]:checked').value;
+  rightContent.innerHTML = ''; // Apagar conteúdo do lado direito
+  console.log(`nome ${name}, sobrenome ${lastName}, email ${emailOrPhone}, data ${birthdate}, genero ${gender}`);
+}
+
+register.addEventListener('click', changeRightContent);

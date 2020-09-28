9dd7aa7c67d029d7a8a0711f8e050aba41a4b11d
jvitorof <j.vitoroflores@gmail.com>
2020-09-28 19:41:07 -0300

    Buscando a Branch Felipe

--- script.js
+++ script.js
@@ -8,30 +8,36 @@ loginButton.addEventListener('click', function () {
 });
 
 genderCustom.addEventListener('click', () => {
+  const customDiv = document.querySelector('#custom');
   const customInput = document.createElement('input');
+  customDiv.innerHTML = '';
   customInput.setAttribute('name', 'gender-custom');
   customInput.classList.add('form-control');
   customInput.placeholder = 'Gênero (opcional)';
-  document.querySelector('#custom').appendChild(customInput);
+  customDiv.appendChild(customInput);
 });
 
 subimitButton.addEventListener('click', function (event) {
   event.preventDefault();
-  const rightForm = document.querySelector('.open-account');
+  // const rightForm = document.querySelector('.open-account');
   const rightContent = document.querySelector('.right-content');
-  const valoresInput = document.querySelectorAll('.verifica');
-  const mensagem = document.createElement('p');
-  rightForm.appendChild(mensagem);
-  for (let index = 0; index < valoresInput.length; index += 1) {
-    if (valoresInput[index].value === '') {
-      mensagem.innerText = 'Campos inválidos';
+  const rightErrorMessage = document.querySelector('#error');
+  const imputedValues = document.querySelectorAll('.verifica');
+  const message = document.createElement('p');
+  rightErrorMessage.innerHTML = '';
+  message.innerText = 'Campos inválidos';
+  message.classList.add('alert');
+  for (let index = 0; index < imputedValues.length; index += 1) {
+    if (imputedValues[index].value === '') {
+      rightErrorMessage.appendChild(message);
     } else {
       const genderChecked = document.querySelectorAll('.custom-radio');
       for (let jindex = 0; jindex < genderChecked.length; jindex += 1) {
         if (genderChecked[jindex].checked) {
-          rightContent.innerHTML = `Olá, ${valoresInput[0].value} ${valoresInput[1].value}
-          ${valoresInput[2].value}
-          ${valoresInput[4].value} ${genderChecked[jindex].value}`;
+          rightContent.innerHTML = `<p> Olá, ${imputedValues[0].value} ${imputedValues[1].value} </p>
+          <p>Email ou telefone: ${imputedValues[2].value} </p>
+          <p>Data de nascimento: ${imputedValues[4].value} </p>
+          <p>Genero: ${genderChecked[jindex].value} </p>`;
         }
       }
     }

c6696e7fdfe98f5326d8cb981e72374d49400f71
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2020-09-28 12:53:22 -0300

    feito a caixa de dialogo do gender

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ document.getElementById('button-login').addEventListener('click', () => {
   const inputText = document.getElementById('user-email-phone');
   alert(inputText.value);
 });
+
+const genderRamdom = document.querySelector('.personalizado');
+function genderGeneration () {
+  if (genderRamdom.checked) {
+    const newElement = document.createElement('input');
+    newElement.name = 'gender-custom';
+    newElement.placeholder = 'Gênero (opcional)';
+    document.querySelector('.input-gender').appendChild(newElement);
+  }
+} // referência http://devfuria.com.br/javascript/manipulando-radios-buttons-com-javascript/
+genderRamdom.addEventListener('click', genderGeneration);

7a24ad1553d40dae82d7cb6dd0601aea33ba788c
Filipi Firmino <contatoic3web@gmail.com>
2020-09-28 12:21:22 -0300

    Correçao na mensagem de retorno

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ cadast.addEventListener('click', () => {
   const formsValue = document.querySelectorAll('.form-newaccaount input');
   for (let i = 0; i < 5; i += 1) {
     if (document.querySelectorAll('input:checked').length < 1 || (inputGender.style.display === 'block' && inputGender.value === '') || formsValue[i].value === '') {
-      document.querySelector('.chek-return').innerHTML = 'Campos invalidos';
+      document.querySelector('.chek-return').innerHTML = 'Campos inválidos';
       break;
     }
   }

7ed507e5919acddafe11f88a597e29ea781be00b
vitorAmorims <vtamorims@gmail.com>
2020-09-27 22:09:37 -0300

    inserido campo genero abaixo de nova senha, caso clique em radio personalizado

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ buttonlogin.addEventListener('click', () => {
 
 const genderP = document.querySelector('#gender-p');
 genderP.addEventListener('change', () => {
-  const newPassword = document.querySelector('.password');
+  const formPassword = document.querySelector('.form-password');
   const inputGender = document.createElement('input');
   inputGender.name = 'gender-custom';
   inputGender.placeholder = 'Gênero (opcional)';
   inputGender.id = 'label-gender';
   inputGender.type = 'text';
-  newPassword.appendChild(inputGender);
+  formPassword.appendChild(inputGender);
 });
 
 let count = 0;/* referência projeto higoramorim-facebook-project*/

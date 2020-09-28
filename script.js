75cdb51ba3b59332a204ddc81a77b49ba2ee56f4
Filipi Firmino <contatoic3web@gmail.com>
2020-09-28 12:01:57 -0300

    Retorno da validaçao dentro do formulario e ajuste no css.

--- script.js
+++ script.js
@@ -34,8 +34,9 @@ cadast.addEventListener('click', () => {
   const formsValue = document.querySelectorAll('.form-newaccaount input');
   for (let i = 0; i < 5; i += 1) {
     if (document.querySelectorAll('input:checked').length < 1 || (inputGender.style.display === 'block' && inputGender.value === '') || formsValue[i].value === '') {
-      alert('Campos inválidos');
-      break;
+      document.querySelector('.chek-return').innerHTML = 'Campos invalidos';
+      return false;
     }
+    
   }
 });

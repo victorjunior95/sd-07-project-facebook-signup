4fbab39fe46945bf35683c08dd986b7efd974e8e
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-09-28 00:58:47 -0300

    Tentativa 01 funçao validar campos

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const button = document.querySelector('#button-login');
 const emailInput = document.querySelector('#user-email-phone');
 const buttonOption = document.querySelectorAll('input[name="gender"]');
 const genderOptions = document.querySelector('.gender-options');
+const signUpButton = document.querySelector('#facebook-register');
 
 button.addEventListener('click', function () {
   const inputValue = emailInput.value;
@@ -15,9 +16,20 @@ for (let index = 0; index < buttonOption.length; index += 1) {
       customInput.type = 'text';
       customInput.placeholder = 'Gênero (opcional)';
       customInput.name = 'gender-custom';
+      customInput.classList.add('form-input');
       genderOptions.appendChild(customInput);
     } else {
       genderOptions.removeChild(genderOptions.lastChild);
     }
   });
 }
+
+signUpButton.addEventListener('click', function () {
+  const forms = document.querySelectorAll('.form-input');
+
+  for (let index = 0; index < forms.length; index += 1) {
+    if (forms[index].value === '') {
+      alert('Campos inválidos');
+    }
+  }
+});

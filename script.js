462cf35dbfce672c483323d096d85f71e577f528
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-09-28 19:00:05 -0300

    Exclusao de variaveis e funcoes para o commit da funcao personalizado

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const button = document.querySelector('#button-login');
 const emailInput = document.querySelector('#user-email-phone');
 const buttonOption = document.querySelectorAll('input[name="gender"]');
 const genderOptions = document.querySelector('.gender-options');
-const signUpButton = document.querySelector('#facebook-register');
 
 button.addEventListener('click', function () {
   const inputValue = emailInput.value;
@@ -23,21 +22,3 @@ for (let index = 0; index < buttonOption.length; index += 1) {
     }
   });
 }
-
-signUpButton.addEventListener('click', function () {
-  const forms = document.querySelectorAll('.form-input');
-
-
-  // Funcao validar campos
-//   for (let index = 0; index < forms.length; index += 1) {
-//     if (forms[index].value === '') {
-//       const invalidField = document.createElement('div');
-
-//       invalidField.innerHTML = 'Campos inválidos';
-
-//       forms
-
-//       // alert('Campos inválidos');
-//     }
-//   }
-// });

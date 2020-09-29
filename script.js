513afcb4eb259405998a25740ec10004185c0c76
barrrosomarcio <barrrosomarcio@gmail.com>
2020-09-28 22:29:02 -0300

    alterações requisito 20

--- script.js
+++ script.js
@@ -49,13 +49,23 @@ document.querySelector('.create-account').addEventListener('submit', function (e
   for (let count = 0; count < formChildren.length; count += 1) {
     formChildren[count].style.display = 'none';
   }
-  const registeredGender = document.querySelector('.form-input:checked').id;
+
   const registeredName = document.querySelector('#first-name').value;
   const registeredLastName = document.querySelector('#last-name').value;
   const registeredPhoneMail = document.querySelector('#phone-email').value;
   const registeredBirthdate = document.querySelector('#birthdate').value;
+  const registeredGender = document.querySelector('.form-input:checked').value;
 
   const showRegistered = document.createElement('p');
-  showRegistered.innerHTML = `Olá, ${registeredName} ${registeredLastName} </br> Phone/Mail: ${registeredPhoneMail} </br> Date of Birth: ${registeredBirthdate} </br> Genero: ${registeredGender}`;
+  showRegistered.innerText = `Olá, ${registeredName} ${registeredLastName}`;
   document.querySelector('.create-account').appendChild(showRegistered);
+  const showPhoneMail = document.createElement('p');
+  showPhoneMail.innerText = `${registeredPhoneMail} `;
+  document.querySelector('.create-account').appendChild(showPhoneMail);
+  const showDateOfBirth = document.createElement('p');
+  showDateOfBirth.innerText = `${registeredBirthdate}`;
+  document.querySelector('.create-account').appendChild(showDateOfBirth);
+  const showGender = document.createElement('p');
+  showGender.innerText = `${registeredGender}`;
+  document.querySelector('.create-account').appendChild(showGender);
 });

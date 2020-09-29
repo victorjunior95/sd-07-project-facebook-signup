5a37ba60cfdf90732a08c20abb7fb94ce2c8fd44
Muniz <vinicius.muniz.marin@gmail.com>
2020-09-29 14:15:40 -0300

    projeto finalizado com todos requisitos

--- script.js
+++ script.js
@@ -10,11 +10,29 @@ radioCustomGender.addEventListener('click', function () {
   const genderCustom = document.createElement('input');
   genderCustom.type = 'text';
   genderCustom.name = 'gender-custom';
+  genderCustom.className = 'account-input';
   genderCustom.placeholder = 'Gênero (opcional)';
   newAccForm.appendChild(genderCustom);
-  newAccForm.insertBefore(genderCustom, newAccForm.children[19]);
+  newAccForm.insertBefore(genderCustom, newAccForm.children[7]);
 });
 
+function checkedGender() {
+  const allGender = document.getElementsByName('gender');
+  let resultado = '';
+  if (allGender[0].checked === true) {
+    resultado = 'Feminino';
+  } else if (allGender[1].checked === true) {
+    resultado = 'Masculino';
+  } else if (allGender[2].checked === true) {
+    resultado = 'Personalizado';
+  }
+  return resultado;
+}
+
+const inputFirstName = document.getElementsByName('firstname')[0];
+const inputLastName = document.getElementsByName('lastname')[0];
+const inputPhoneEmail = document.getElementsByName('phone_email')[0];
+const inputBirthdate = document.getElementsByName('birthdate')[0];
 
 const validate = new window.JustValidate('.new-account', {
   messages: {
@@ -51,8 +69,25 @@ const validate = new window.JustValidate('.new-account', {
     radio: {
       required: true,
     },
+    password: {
+      strength: {
+        custom: '[a-zA-Z0-9]',
+      },
+    },
   },
 
+  submitHandler() {
+    const divRightContent = document.getElementsByClassName('right-content')[0];
+    const fullName = `${inputFirstName.value} ${inputLastName.value}`;
+    const saudacoes = `Olá, ${fullName}
+${inputPhoneEmail.value}
+${inputBirthdate.value}
+${checkedGender()}`;
+    const divSaudacoes = document.createElement('div');
+    divSaudacoes.innerText = saudacoes;
+    divRightContent.innerHTML = '';
+    divRightContent.appendChild(divSaudacoes);
+  },
 });
 
 window.onload = function () {

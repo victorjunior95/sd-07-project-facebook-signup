b6ad20ebab62bb22e0ac331b6dc77aa439622712
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2020-09-29 13:37:49 -0300

    issue

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ document.getElementById('button-login').addEventListener('click', () => {
   alert(inputText.value);
 });
 
-const genderRamdom = document.querySelector(".personalizado");
+const genderRamdom = document.querySelector('.personalizado');
 function genderGeneration() {
   if (genderRamdom.checked) {
-    const newElement = document.createElement("input");
-    newElement.name = "gender-custom";
-    newElement.placeholder = "Gênero (opcional)";
-    document.querySelector(".input-gender").appendChild(newElement);
+    const newElement = document.createElement('input');
+    newElement.name = 'gender-custom';
+    newElement.placeholder = 'Gênero (opcional)';
+    document.querySelector('.input-gender').appendChild(newElement);
   }
 } // referência http://devfuria.com.br/javascript/manipulando-radios-buttons-com-javascript/
-genderRamdom.addEventListener("click", genderGeneration);
+genderRamdom.addEventListener('click', genderGeneration);

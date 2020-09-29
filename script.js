e5184b315230d097132d6010f045b42e89ee2c8c
Renan OLiveira <ren4n.dev@gmail.com>
2020-09-29 11:02:34 -0300

    Terminando requisito 20

--- script.js
+++ script.js
@@ -6,9 +6,23 @@ const lastname = document.getElementById('lastname');
 const phoneEmail = document.getElementById('phone_email');
 const password = document.getElementById('password');
 const birthdate = document.getElementById('birthdate');
-const cadastro = document.getElementById('facebook-register');
+const feminino = document.getElementById('feminino');
+const masculino = document.getElementById('masculino');
 const personalizado = document.getElementById('genero');
+const cadastro = document.getElementById('facebook-register');
 const genderInput = document.querySelector('#input-personalizado');
+
+function getGender() {
+  let gender = '';
+  if (feminino.checked) {
+    gender = 'Feminino';
+  } else if (masculino.checked) {
+    gender = 'Masculino';
+  } else if (personalizado.checked) {
+    gender = 'Personalizado';
+  }
+  return gender;
+}
 
 login.addEventListener('click', function () {
   alert(user.value);
@@ -17,8 +31,21 @@ login.addEventListener('click', function () {
 cadastro.addEventListener('click', function (event) {
   event.preventDefault();
   const spanInvalid = document.querySelector('#invalid');
-  if ((name.value === '') || (lastname.value === '') || (phoneEmail.value === '') || (password.value === '') || (birthdate.value === '')) {
+  const person = {
+    name: name.value,
+    lastname: lastname.value,
+    phoneEmail: phoneEmail.value,
+    password: password.value,
+    birthdate: birthdate.value,
+    gender: getGender(),
+  };
+  if ((person.name === '') || (person.lastname === '') || (person.phoneEmail === '') || (person.password === '') || (person.birthdate === '')) {
     spanInvalid.innerHTML = 'Campos inválidos';
+  } else {
+    document.querySelector('.right-content').innerHTML = `<p>Olá, ${person.name} ${person.lastname}</p>
+    <p>${person.phoneEmail}</p>
+    <p>${person.birthdate}</p>
+    <p>${person.gender}</p>`;
   }
 });
 

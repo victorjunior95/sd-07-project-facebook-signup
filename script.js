95048dd581c263dbbec2f85a19ece02446dc783c
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-09-28 22:39:15 -0300

    Mais uma tentativa de validação

--- script.js
+++ script.js
@@ -5,3 +5,36 @@ function alertButton() {
   }
 }
 document.getElementById('button-login').addEventListener('click', alertButton);
+
+function radioCheked() {
+  const otherGender = document.getElementById('other');
+  const novoInput = document.createElement('input');
+  const inputsValue = document.querySelectorAll('.u-gender-option input');
+  if ((otherGender.checked) && (inputsValue.length < 4)) {
+    novoInput.setAttribute('id', 'gender-custom');
+    novoInput.setAttribute('placeholder', 'Gênero (opcional)');
+    document.querySelector('.u-gender-option').appendChild(novoInput);
+  }
+}
+document.getElementById('other').addEventListener('click', radioCheked);
+
+const singUpInputs = document.querySelectorAll('.sing-up input');
+function emptyInput(event) {
+  event.preventDefault();
+  const errorAlert = document.createElement('p');
+  document.querySelector('.sing-up').appendChild(errorAlert);
+  for (let count = 0; count < 5; count += 1) {
+    if (singUpInputs[count].value === '') {
+      errorAlert.innerText = 'Campos inválidos';
+    } else {
+      const firstName = document.getElementById('firstname').value;
+      const lastName = document.getElementById('lastname').value;
+      const phoneMail = document.getElementById('phone_email').value;
+      const birthDate = document.getElementById('birthdate').value;
+      const gender = document.querySelector('input[name="gender"]').value;
+      const welcomeUser = `Olá, ${firstName} ${lastName} ${gender} ${phoneMail} ${birthDate}`;
+      document.querySelector('.right-content').innerHTML = `${welcomeUser}`;
+    }
+  }
+}
+document.getElementById('facebook-register').addEventListener('click', emptyInput);

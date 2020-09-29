65629881e3ad7b54a56fb4c3d9225e4989c0edc5
Thays Costa <thayscosta3@gmail.com>
2020-09-29 04:20:17 -0300

    Requisito obrigatorio nº 18 - valida se os campos estao preenchidos ao clicar em 'Cadastre-se'.

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ buttonLogin.addEventListener('click', function () {
   const text = userEmailPhone.value;
   alert(text);
 });
+
+const buttonRegister = document.getElementById('facebook-register');
+buttonRegister.addEventListener('click', function () {
+  const fields = document.getElementsByTagName('input');
+  for (let index = 0; index < fields.length; index += 1) {
+    if (fields[index].value === '') {
+      const form = document.getElementById('form2');
+      form.innerHTML = 'Campos Inválidos';
+    }
+  }
+});

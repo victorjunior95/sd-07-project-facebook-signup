470225c2e41e08901d121ea3e5a321b42982d69f
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-09-28 21:51:54 -0300

    Commit: 0.3.5

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 const divRadios = document.querySelector('.radios-container');
 const name = document.getElementById('name');
-// const lastname = document.getElementById('lastname');
-// const phoneEmail = document.getElementById('phoneEmail');
+const lastname = document.getElementById('lastname');
+const phoneEmail = document.getElementById('phoneEmail');
+const birthday = document.getElementById('date');
+const rightContent = document.querySelector('.right-content');
 
 document.getElementById('button-login').addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
 });
 
+function clearChilds(father) {
+  while (father.firstChild) {
+    father.removeChild(father.firstChild);
+  }
+};
+
+function getValueRadios() {
+  const radios = document.querySelectorAll('.inputRadios');
+  for (let index = 0; index < radios.length; index += 1) {
+    if (radios[index].checked) {
+      return radios[index].value;
+    }
+  }
+  return false;
+}
+
+function insertValuesParagraph(valueRadio) {
+  const p = document.createElement('p');
+  p.innerText = `Olá, ${name.value} ${lastname.value}`;
+  p.innerText += phoneEmail.value;
+  p.innerText += birthday.value;
+  p.innerText += valueRadio;
+  rightContent.appendChild(p);
+}
+
 document.getElementById('facebook-register').addEventListener('click', function () {
-  alert(name.value);
+  const valueRadio = getValueRadios();
+  clearChilds(rightContent);
+  insertValuesParagraph(valueRadio);
 });
 
 const validate = new window.JustValidate('.name', {

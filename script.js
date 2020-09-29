616be2cea559c63821c1746a0a80a6b7a3a7645b
Luise Rios <luisemiranda@gmail.com>
2020-09-29 13:19:03 -0300

    requisitos 18 e 20

--- script.js
+++ script.js
@@ -16,3 +16,44 @@ genderSelect.addEventListener('click', function () {
   inputCustom.placeholder = 'Gênero (opcional)';
   customDiv.appendChild(inputCustom);
 });
+
+const female = document.querySelector('#female');
+const male = document.querySelector('#male');
+
+function genderOption() {
+  let gender = 'Personalizado';
+  if (female.checked === true) {
+    gender = 'Feminino';
+  } else if (male.checked === true) {
+    gender = 'Masculino';
+  }
+  return gender;
+}
+
+const inputForm = document.querySelectorAll('.signup input');
+
+function validation() {
+  const rightContent = document.querySelector('.right-content');
+  rightContent.innerHTML = `Olá, ${inputForm[0].value} ${inputForm[1].value} <br>
+  ${inputForm[2].value} <br>
+  ${inputForm[4].value} <br>
+  ${genderOption()}`;
+}
+
+const signupButton = document.querySelector('#facebook-register');
+const signup = document.querySelector('.signup');
+
+signupButton.addEventListener('click', function (event) {
+  let valid = true;
+  event.preventDefault();
+  inputForm.forEach((element) => {
+    if (element.value === '') {
+      valid = false;
+    }
+  });
+  if (valid === false) {
+    signup.innerHTML = 'Campos inválidos';
+  } else {
+    validation();
+  }
+});

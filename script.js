54428c6a93995c0f3edfbb8f82f3531cc740cea5
Filipi Firmino <contatoic3web@gmail.com>
2020-09-29 15:12:28 -0300

    correcao CC

--- script.js
+++ script.js
@@ -47,12 +47,12 @@ cadast.addEventListener('click', () => {
   const formsValue = document.querySelectorAll('.form-newaccaount input');
   const input = document.querySelectorAll('input:checked').length;
   for (let i = 0; i < 5; i += 1) {
-  if (input < 1 ||
+    if (input < 1 ||
   (inputGender.style.display === 'block' && inputGender.value === '')
   || formsValue[i].value === '') {
     document.querySelector('.chek-return').style.display = 'block';
   } else {
     welcome();
   }
-}
+  }
 });

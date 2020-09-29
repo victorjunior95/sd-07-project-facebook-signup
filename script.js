6679c7b5fded19eb0fac81667109144d3e93815f
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-09-29 00:33:25 -0300

    estilizando a pagina para se parecer mais com o original

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const loginButton = document.getElementById('button-login');
+const loginButton = document.getElementById('login-button');
 
 loginButton.addEventListener('click', function () {
-  const emailInput = document.getElementById('user-email-phone').value;
+  const emailInput = document.getElementById('email-input').value;
   alert(emailInput);
 });
 
@@ -9,33 +9,31 @@ const enrrolmentButton = document.getElementById('facebook-register');
 
 enrrolmentButton.addEventListener('click', function (event) {
   event.preventDefault();
-  const inputList = document.getElementsByClassName('form-field');
-
-  for (let index = 0; index < inputList.length; index += 1) {
-    if (inputList[index].value === '') {
-      document.getElementById('invalid-field').style.display = 'block';
-      return;
-    }
-  }
-  const formData = document.getElementById('form-data');
-  document.getElementById('sign-up-completed').classList.remove('gender-personalized');
-  document.getElementById('sign-up-form').classList.add('gender-personalized');
+  const inputList = document.querySelectorAll('.signup-form input');
+  // for (let index = 0; index < inputList.length; index += 1) {
+  //   if (inputList[index].value === '') {
+  //     document.getElementById('hidden-input').classList.remove('hidden-input');
+  //     return;
+  //   }
+  // }
+  const data = document.getElementById('signup-form');
+  console.log(data.gender);
   const outputList = document.getElementsByClassName('output-form');
   const outputDiv = document.getElementById('sign-up-completed');
-  outputDiv.innerHTML = `Olá, ${outputList[0].value} ${outputList[1].value}<br>
-  Email: ${outputList[2].value}<br>
-  Data de nascimento: ${outputList[4].value}<br>
-  Gênero: ${formData.gender.value}`;
+  // outputDiv.innerHTML = `Olá, ${outputList[0].value} ${outputList[1].value}<br>
+  // Email: ${outputList[2].value}<br>
+  // Data de nascimento: ${outputList[4].value}<br>
+  // Gênero: ${data.gender.value}`;
 });
 
-document.getElementById('other').addEventListener('click', function () {
-  document.getElementById('gender-personalized').classList.remove('gender-personalized');
-});
+document.getElementById('custom-radio').addEventListener('click', () => {
+  document.getElementById('custom-options').classList.remove('hidden-input');
+})
 
-document.getElementById('female').addEventListener('click', function () {
-  document.getElementById('gender-personalized').classList.add('gender-personalized');
-});
+document.getElementById('male-radio').addEventListener('click', () => {
+  document.getElementById('custom-options').classList.add('hidden-input')
+})
 
-document.getElementById('male').addEventListener('click', function () {
-  document.getElementById('gender-personalized').classList.add('gender-personalized');
-});
+document.getElementById('female-radio').addEventListener('click', () => {
+  document.getElementById('custom-options').classList.add('hidden-input')
+})

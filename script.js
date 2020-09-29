30bcc070ae91b1493bfcc6c6707c14857295b973
Eriquy Jhordan <eriquy@gmail.com>
2020-09-29 14:42:00 -0300

    alterando o script e add div nos radios

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 const login = document.getElementById('button-login');
-const gender = document.querySelector('.gender');
+const gender = document.querySelector('#custom');
 
 login.addEventListener('click', () => {
   const email = document.getElementById('user-email-phone').value;
   alert(`${email}`);
 });
 
-gender.addEventListener('click', () => {
-  for (let i = 0; i < gender.children.length; i += 1) {
-    if (gender.children[i].checked) {
-      const selectedElement = gender.children[i];
-      if (selectedElement.value === 'Personalizado' && gender.children.length <= 7) {
-        const text = document.createElement('input');
-        text.name = 'gender-custom';
-        text.placeholder = 'Gênero (opcional)';
-        gender.appendChild(text);
-      } else if (selectedElement.value !== 'Personalizado' && gender.children.length > 7) {
-        gender.removeChild(gender.lastElementChild);
-      }
-    }
-  }
+gender.addEventListener('change', () => {
+  const form  = document.querySelector('.gender');
+  const text = document.createElement('input');
+  text.name = 'gender-custom';
+  text.placeholder = 'Gênero (opcional)';
+  form.appendChild(text);
 });

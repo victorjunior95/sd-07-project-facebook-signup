c820679c8933331f9f13fab3910728fea01cc058
Eduardo Souza <dado.eletrons@gmail.com>
2020-09-30 02:47:58 -0300

    realizado 19°

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const callAlertValueInput = () => {
 // Error messagens forms empty
 const showMessagerErrorForm = () => {
   const pElement = document.querySelector('.errorField');
-  if (pElement.classList.contains('isMensager')) {
-    pElement.classList.remove('isMensager');
+  if (pElement.classList.contains('isMensagerDisplayNone')) {
+    pElement.classList.remove('isMensagerDisplayNone');
     setTimeout(() => {
-      pElement.classList.add('isMensager');
-    }, 5000);
+      pElement.classList.add('isMensagerDisplayNone');
+    }, 2000);
   }
 };
 
@@ -44,6 +44,18 @@ const validateFormInput = () => {
   });
 };
 
+const showInputGenderCustom = (event) => {
+  const textInput = document.querySelector('input[name="gender-custom"]');
+  const radioInputValue = event.target.value;
+  if (radioInputValue === 'Personalizado') {
+    if (textInput.classList.contains('isInputDisplayNone')) {
+      textInput.classList.remove('isInputDisplayNone');
+    }
+  } else if (!textInput.classList.contains('isInputDisplayNone')) {
+    textInput.classList.add('isInputDisplayNone');
+  }
+};
+
 const controllerEventsClicks = (type) => {
   document.addEventListener(type, (event) => {
     const eventDataSet = event.target.dataset.click;
@@ -53,6 +65,9 @@ const controllerEventsClicks = (type) => {
         break;
       case 'submit':
         validateFormInput();
+        break;
+      case 'gender':
+        showInputGenderCustom(event);
         break;
 
       default:

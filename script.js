7f6365fcde05f36d94cd008e422eadd41b66100d
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-09-30 14:57:27 -0300

    teste

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-const loginBtn = document.querySelector('#button-login');
-const userEmailPhone = document.querySelector('#user-email-phone');
-const submitButton = document.querySelector('#facebook-register');
-const fisrtName = document.querySelector('#firstName');
-const lastName = document.querySelector('#lastName');
-const phoneEmail = document.querySelector('#phoneEmail');
-const password = document.querySelector('#password');
-const birthDate = document.querySelector('#birthdate');
+const loginBtn = document.querySelector("#button-login");
+const userEmailPhone = document.querySelector("#user-email-phone");
+const submitButton = document.querySelector("#facebook-register");
+const fisrtName = document.querySelector("#firstName");
+const lastName = document.querySelector("#lastName");
+const phoneEmail = document.querySelector("#phoneEmail");
+const password = document.querySelector("#password");
+const birthDate = document.querySelector("#birthdate");
 let textChecked = true;
 
 const inputTextsValidation = () => {
-  if (fisrtName.value === '' || lastName.value === '') {
+  if (fisrtName.value === "" || lastName.value === "") {
     textChecked = false;
   }
 
-  if (phoneEmail.value === '' || password.value === '' || birthDate.value === '') {
+  if (
+    phoneEmail.value === "" ||
+    password.value === "" ||
+    birthDate.value === ""
+  ) {
     textChecked = false;
   }
   return textChecked;
 };
 
-const femaleRadio = document.querySelector('#female');
-const maleRadio = document.querySelector('#male');
-const otherRadio = document.querySelector('#other');
+const femaleRadio = document.querySelector("#female");
+const maleRadio = document.querySelector("#male");
+const otherRadio = document.querySelector("#other");
 let radioChecked = false;
 
 const inputRadioValidation = () => {
@@ -31,16 +35,16 @@ const inputRadioValidation = () => {
   return radioChecked;
 };
 
-const otherGender = document.querySelector('#otherGender');
+const otherGender = document.querySelector("#otherGender");
 
 const validateForm = () => {
   event.preventDefault();
   inputTextsValidation();
   inputRadioValidation();
 
-  const errorMessage = document.querySelector('#errorMessage');
-  const mainContent = document.querySelector('#right-content');
-  const genderChoose = document.querySelector('[type=radio]:checked');
+  const errorMessage = document.querySelector("#errorMessage");
+  const mainContent = document.querySelector("#right-content");
+  const genderChoose = document.querySelector("[type=radio]:checked");
 
   if (textChecked && radioChecked) {
     mainContent.innerHTML = `Olá, ${fisrtName.value} ${lastName.value}
@@ -48,18 +52,18 @@ const validateForm = () => {
     ${birthDate.value}
     ${genderChoose.value}`;
   } else {
-    errorMessage.innerText = 'Campos inválidos';
-  } 
+    errorMessage.innerText = "Campos inválidos";
+  }
 };
 
 window.onload = function () {
-  loginBtn.addEventListener('click', () => alert(userEmailPhone.value));
-  submitButton.addEventListener('click', validateForm);
-  otherRadio.addEventListener('change', () => {
+  loginBtn.addEventListener("click", () => alert(userEmailPhone.value));
+  submitButton.addEventListener("click", validateForm);
+  otherRadio.addEventListener("change", () => {
     if (otherRadio.checked) {
-      otherGender.style.display = 'block';
+      otherGender.style.display = "block";
     } else {
-      otherGender.style.display = 'none';
+      otherGender.style.display = "none";
     }
   });
 };

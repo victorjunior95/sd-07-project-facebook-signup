951cbb4017997103eeb4f58b161bbc4169634911
Thays Costa <thayscosta3@gmail.com>
2020-09-30 03:25:12 -0300

    Alteraçoes no requisito obrigatorio nº 20 - retirando o requisito de dentro do else (substitui conteudo da section 'right-content').

--- script.js
+++ script.js
@@ -11,22 +11,23 @@ buttonRegister.addEventListener('click', function (event) {
   event.preventDefault();
   const fields = document.querySelectorAll('#form2 input');
   for (let index = 0; index < fields.length; index += 1) {
-    const form = document.getElementById('form2');
     if (fields[index].value === '') {
+      const form = document.getElementById('form2');
       form.innerHTML = 'Campos inválidos';
-    } else {
-      const name = document.getElementById('firstname').value;
-      const lastName = document.getElementById('lastname').value;
-      const fullName = `${name} ${lastName}`;
-      const email = document.getElementById('phone_email').value;
-      const birthDate = document.getElementById('birthdate').value;
-      const gender = document.querySelectorAll('input .gender').value;
-      const rightContent = document.getElementById('right-content');
-      rightContent.innerHTML = `Olá, ${fullName}.
-      Seu email é ${email}.
-      Sua data de nascimento é ${birthDate}.
-      Seu gênero é ${gender}`;
     }
+    // } else {
+    const name = document.getElementById('firstname').value;
+    const lastName = document.getElementById('lastname').value;
+    const fullName = `${name} ${lastName}`;
+    const email = document.getElementById('phone_email').value;
+    const birthDate = document.getElementById('birthdate').value;
+    const gender = document.querySelectorAll('input .gender').value;
+    const rightContent = document.getElementById('right-content');
+    rightContent.innerHTML = `Olá, ${fullName}.
+    Seu email é ${email}.
+    Sua data de nascimento é ${birthDate}.
+    Seu gênero é ${gender}`;
+    // }
   }
 });
 

29768d35041399cf59c5befe7e3a7f30c65e05fa
ericvini <ericcobain3@gmail.com>
2020-09-29 23:55:16 -0300

    Refatorandpo requisto 18

--- script.js
+++ script.js
@@ -12,34 +12,27 @@ const inputDataNascimento = document.getElementById('data-nascimento');
 const radioFeminino = document.getElementById('female');
 const radioMasculino = document.getElementById('male');
 const radioPersonalizado = document.getElementById('other');
-const labelGender = document.getElementById('label-gender');
-const labelLabelBirthdate = document.getElementById('label-birthdate');
+const campoInvalido = document.getElementById('campo-invalido');
 
 const btnCadastro = document.getElementById('facebook-register');
 btnCadastro.addEventListener('click', function () {
   event.preventDefault();
 
   if (inputFirstname.value === '') {
-    inputFirstname.value = 'campos inválidos';
-  }
-  if (inputLastname.value === '') {
-    inputLastname.value = 'campos inválidos';
-  }
-  if (inputPhoneEmail.value === '') {
-    inputPhoneEmail.value = 'campos inválidos';
-  }
-  if (inputPassword.value === '') {
-    inputPassword.value = 'campos inválidos';
-  }
-  if (inputDataNascimento.value === '') {
-    labelLabelBirthdate.innerHTML = 'campos inválidos';
-  }
-
-  if (
+    campoInvalido.innerHTML = 'campos inválidos';
+  } else if (inputLastname.value === '') {
+    campoInvalido.innerHTML = 'campos inválidos';
+  } else if (inputPhoneEmail.value === '') {
+    campoInvalido.innerHTML = 'campos inválidos';
+  } else if (inputPassword.value === '') {
+    campoInvalido.innerHTML = 'campos inválidos';
+  } else if (inputDataNascimento.value === '') {
+    campoInvalido.innerHTML = 'campos inválidos';
+  } else if (
     radioFeminino.checked === false &&
     radioMasculino.checked === false &&
     radioPersonalizado.checked === false
   ) {
-    labelGender.innerHTML = 'campos inválidos';
+    campoInvalido.innerHTML = 'campos inválidos';
   }
 });

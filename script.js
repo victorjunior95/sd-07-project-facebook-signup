cb3592e4c7b9caac46b6f99fe29f8506032edb0f
Francisco <franciscoliratorres@gmail.com>
2020-09-30 14:11:56 -0300

    Add vltando aos 85

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const inputValue = document.getElementById('user-email-phone');
 buttonLogin.addEventListener('click', function () {
   alert(inputValue.value);
 });
+
+const buttonCustomGender = document.getElementById('personalizado');
+const formBox = document.getElementById('form-box');
+const facebookRegister = document.getElementById('facebook-register');
+
+buttonCustomGender.addEventListener('click', function () {
+  const genderCustom = document.createElement('input');
+  genderCustom.className = 'form-input-big';
+  genderCustom.setAttribute('name', 'gender-custom');
+  genderCustom.setAttribute('placeholder', 'Gênero');
+  formBox.insertBefore(genderCustom, facebookRegister);
+});

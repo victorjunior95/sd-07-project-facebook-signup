67683e1174fec6f089657d051a8a4d5baa380f24
MatheuzaoCarvalho <matheusvscarvalho@gmail.com>
2020-09-30 13:59:36 -0300

    Reestilizando o CSS e Alterando Java-2 tentativa

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// function clickButton() {
-//   const userEmailPhone = document.getElementById('user-email-phone').value;
-//   if (userEmailPhone !== '') {
-//     alert(userEmailPhone);
-//   }
-// }
-// const button = document.getElementById('button-login');
-// button.addEventListener('click', clickButton); 
-
 const buttonLogin = document.querySelector('#button-login');
 const emailValidation = document.querySelector('#user-email-phone');
 

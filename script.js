0da194d0221354d6cf4243a0d05714a49acf5145
richardfazeredo <richardcompletetour@gmail.com>
2020-09-29 22:08:39 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const buttonLogin = document.querySelector('button');
+const buttonSignUp = document.getElementById('facebook-register');
 
 buttonLogin.addEventListener('click', function () {
   const login = document.querySelector('input');
   alert(login.value);
   login.innerHTML = '';
+});
+
+buttonSignUp.addEventListener('click', () => {
+  let counterEmptyField = 0;
+  const formControl = document.querySelectorAll('#sign-up-form input');
+  formControl.forEach(function (form) {
+    if (form.value === '' || (form.type === 'radio') !== form.checked) {
+      counterEmptyField += 1;
+    }
+  });
+  if (counterEmptyField > 2) {
+    alert('Campos inválidos');
+  }
 });

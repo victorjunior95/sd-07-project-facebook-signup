7a56c57ec9119e3f83acb2905c395652af360088
Eriquy Jhordan <eriquy@gmail.com>
2020-09-30 10:42:34 -0300

    refatorando o script

--- script.js
+++ script.js
@@ -1,60 +1,39 @@
 const login = document.getElementById('button-login');
-const genderSelect = document.getElementById('custom');
-const buttonSubmit = document.querySelector('#facebook-register');
-
-function checkEmpty(element) {
-  if (element.value === '') {
-    return true;
-  }
-  return false;
-}
-
-function checkRadios(radios) {
-  for (let i = 0; i < radios.length; i += 1) {
-    if (radios[i].checked) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function data() {
-  const name = document.getElementById('name').value;
-  const lastName = document.getElementById('lastname').value;
-  const email = document.getElementById('email-or-phone').value;
-  const birthDate = document.getElementById('birth').value;
-  const genderOptions = document.querySelectorAll('.selected-radio');
-  let gender = '';
-  for (let i = 0; i < genderOptions.length; i += 1) {
-    if (genderOptions[i].checked) {
-      gender = genderOptions[i].value;
-    }
-  }
-  const user = {
-    name,
-    lastName,
-    email,
-    birthDate,
-    gender,
-  };
-  return user;
-}
-
-function welcome(user) {
-  const rightContent = document.querySelector('.right-content');
-  rightContent.innerHTML = `<p>Olá, ${user.name} ${user.lastName}</p>`;
-  rightContent.innerHTML += `<p>${user.email}</p>`;
-  rightContent.innerHTML += `<p>${user.birthDate}</p>`;
-  rightContent.innerHTML += `<p>${user.gender}</p>`;
-}
-
-
 login.addEventListener('click', () => {
-  const email = document.getElementById('user-email-phone').value;
-  alert(`${email}`);
+  return alert(`${document.getElementById('user-email-phone').value}`);
 });
-
-genderSelect.addEventListener('click', () => {
+const buttonSubmit = document.querySelector('#facebook-register');
+function checkRadioButton() {
+  const gender = document.querySelectorAll('.selected-radio');
+  for (let i = 0; i < gender.length; i += 1) {
+    if (gender[i].checked) {
+      return gender[i].value;
+    }
+  }
+  return false;
+}
+function submit(e) {
+  e.preventDefault();
+  const inputsRequired = document.querySelectorAll('.valid');
+  const gender = checkRadioButton();
+  for (let i = 0; i < inputsRequired.length; i += 1) {
+    if (inputsRequired[i].value === '' || !gender) {
+      const error = document.createElement('p');
+      error.innerText = 'Campos inválidos';
+      error.style.color = 'red';
+      const form = document.querySelector('.form-register');
+      return form.appendChild(error);
+    }
+  }
+  const name = document.querySelector('#name').value;
+  const lastName = document.querySelector('#lastname').value;
+  const phoneOrEmail = document.querySelector('#email-or-phone').value;
+  const birth = document.querySelector('#birth').value;
+  return document.querySelector('.right-content').innerHTML = `Olá, ${name} ${lastName} ${gender} ${phoneOrEmail} ${birth}`;
+}
+buttonSubmit.addEventListener('click', submit);
+const genderSelect = document.getElementById('custom');
+function addCustom() {
   if (genderSelect.checked) {
     const containerCustom = document.getElementById('custom-input');
     const text = document.createElement('input');
@@ -62,25 +41,5 @@ genderSelect.addEventListener('click', () => {
     text.placeholder = 'Gênero (opcional)';
     containerCustom.appendChild(text);
   }
-});
-
-buttonSubmit.addEventListener('click', (e) => {
-  e.preventDefault();
-  const inputs = document.querySelectorAll('.valid');
-  const radios = document.querySelectorAll('.selected-radio');
-  let inputValid = true;
-  for (let i = 0; i < inputs.length; i += 1) {
-    if (checkEmpty(inputs[i])) {
-      inputValid = false;
-    }
-  }
-  if (inputValid && checkRadios(radios)) {
-    const user = data();
-    return welcome(user);
-  }
-  const form = document.querySelector('.form-register');
-  const error = document.createElement('p');
-  error.innerHTML = 'Campos inválidos';
-  error.style.color = 'red';
-  return form.appendChild(error);
-});
+}
+genderSelect.addEventListener('click', addCustom);

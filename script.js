9a93c156fbb536e3065f05febf86ed08acb2cbb4
Thays Costa <thayscosta3@gmail.com>
2020-10-01 02:01:18 -0300

    Function para capturar o gender selecionado e recuperar requisito nº 18 - agora vai mesmo!

--- script.js
+++ script.js
@@ -21,11 +21,12 @@ buttonRegister.addEventListener('click', function (event) {
   const birthDate = document.getElementById('birthdate');
   const rightContent = document.getElementById('right-content');
   const fields = document.querySelectorAll('#form2 input');
+  const form = document.getElementById('form2');
   for (let index = 0; index < fields.length; index += 1) {
     if (fields[index].value === '') {
       const error = document.createElement('p');
       error.innerText = 'Campos inválidos';
-      document.getElementById('form2').appendChild(error);
+      form.appendChild(error);
       break;
     } else {
       rightContent.innerHTML = `Olá, ${firstName.value} ${lastName.value}.

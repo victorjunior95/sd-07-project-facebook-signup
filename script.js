bd2e6f17b9f24dff1017a1b97201e51b9e392150
Lorena Góes <goesloren@gmail.com>
2020-10-01 13:49:02 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const buttonLogin = document.querySelector('button');
 const buttonSignUp = document.getElementById('facebook-register');
 const selectOtherGender = document.querySelector('#other');
+let counterEmptyField = 0;
 
 buttonLogin.addEventListener('click', function () {
   const login = document.querySelector('input');
@@ -8,27 +9,37 @@ buttonLogin.addEventListener('click', function () {
   login.innerHTML = '';
 });
 
-buttonSignUp.addEventListener('click', (e) => {
-  e.preventDefault();
-  let counterEmptyField = 0;
+const formCheck = () => {
   const formControl = document.querySelectorAll('#sign-up-form input');
   formControl.forEach(function (form) {
     if (form.value === '' || (form.type === 'radio') !== form.checked) {
       counterEmptyField += 1;
     }
   });
+};
+
+const helloUser = () => {
+  const rightContent = document.querySelector('#right-content');
+  rightContent.innerHTML = '';
+};
+
+buttonSignUp.addEventListener('click', (e) => {
+  e.preventDefault();
+  formCheck();
 
   if (counterEmptyField > 2) {
+    const form = document.querySelector('#sign-up-form');
+
     const p = document.createElement('p');
-    document.querySelector('#sign-up-form')
-    .appendChild(p).innerText = 'Campos inválidos';
+    form.appendChild(p).innerText = 'Campos inválidos';
+  } else {
+    helloUser();
   }
 });
 
 selectOtherGender.addEventListener('click', () => {
   const divGender = document.querySelector('#other-gender');
   const inputGender = document.createElement('input');
-
   if (!divGender.firstChild) {
     divGender.appendChild(inputGender);
     inputGender.setAttribute('name', 'gender-custom');

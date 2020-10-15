fe17f7b35a25278000ee856536ff253305a18113
Geziel <geziel.fernandez@pucp.pe>
2020-10-15 18:48:55 -0300

    final requisites

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // const button = document.getElementById('button-login');
 const other = document.getElementById('personalizado');
+const father = document.getElementById('signup-form');
 
 other.addEventListener('change', () => {
-  const father = document.getElementById('signup-form');
   const customGender = document.createElement('input');
   customGender.type = 'text';
   customGender.name = 'gender-custom';
@@ -18,4 +18,32 @@ document.getElementById('button-login').addEventListener('click', () => {
   const email = document.getElementById('user-email-phone').value;
   const password = document.getElementById('user-password').value;
   alert(email + password);
+});
+
+// Botão cadastro
+const cadastro = document.getElementById('facebook-register');
+
+
+cadastro.addEventListener('click', (event) => {
+  event.preventDefault();
+  const elements = [...document.querySelectorAll('#signup-form input')];
+  // console.log(elements);
+  let contador = 0;
+  for (let i = 0; i < 5; i += 1) {
+    if (elements[i].value === '') {
+      // console.log(i);
+      contador += 1;
+    }
+  }
+  if (contador > 0) {
+    father.innerHTML += '<p>Campos inválidos</p>';
+  }
+  const inputName = elements[0].value;
+  const inputLastName = elements[1].value;
+  const phoneNumber = elements[2].value;
+  const dateBirth = elements[4].value;
+  const genderPer = document.querySelector('input[type=radio]:checked').value;
+  // console.log(`${inputName} ${inputLastName} ${phoneNumber} ${dateBirth} ${genderPer}`);
+
+  document.querySelector('.right-content').innerHTML = `Olá, ${inputName} ${inputLastName} ${phoneNumber} ${dateBirth} ${genderPer}`;
 });

9d2e49631b38c07a9121495f9f2cdb4f53b5b5f7
Francisco <franciscoliratorres@gmail.com>
2020-10-17 16:34:02 -0300

    Primeira tentativa até requisito 18

--- script.js
+++ script.js
@@ -2,3 +2,56 @@ const buttonLogin = document.getElementById('button-login');
 const inputValue = document.getElementById('user-email-phone');
 
 buttonLogin.addEventListener('click', () => alert(inputValue.value));
+
+const validateFields = {
+  firstname: {
+    required: true,
+  },
+  lastname: {
+    required: true,
+    maxLength: 20,
+  },
+  phone_email: {
+    required: true,
+  },
+  password: {
+    required: true,
+  },
+  birthdate: {
+    required: true,
+  },
+  gender: {
+    required: true,
+  },
+};
+
+const warnings = {
+  firstname: {
+    required: 'Campos inválidos',
+  },
+  lastname: {
+    required: 'Campos inválidos',
+  },
+  phone_email: {
+    required: 'Campos inválidos',
+  },
+  password: {
+    required: 'Campos inválidos',
+  },
+  birthdate: {
+    required: 'Campos inválidos',
+  },
+  gender: {
+    required: 'Campos inválidos',
+  },
+};
+
+const rulesValidate = {
+  rules: validateFields,
+  messages: warnings,
+};
+
+window.onload = function () {
+  const formRules = new window.JustValidate('.form-rules', rulesValidate);
+  return formRules;
+};

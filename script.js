6e33f4767492acbc4cacb5109fb780f689dc3025
Francisco <franciscoliratorres@gmail.com>
2020-10-19 10:46:06 -0300

    tentativa requisito 18

--- script.js
+++ script.js
@@ -3,55 +3,38 @@ const inputValue = document.getElementById('user-email-phone');
 
 buttonLogin.addEventListener('click', () => alert(inputValue.value));
 
-const validateFields = {
-  firstname: {
-    required: true,
-  },
-  lastname: {
-    required: true,
-    maxLength: 20,
-  },
-  phone_email: {
-    required: true,
-  },
-  password: {
-    required: true,
-  },
-  birthdate: {
-    required: true,
-  },
-  gender: {
-    required: true,
-  },
-};
 
-const warnings = {
-  firstname: {
-    required: 'Campos inválidos',
+const validate = new window.JustValidate('.js-form', {
+  rules: {
+    firstname: {
+      required: true,
+    },
+    lastname: {
+      required: true,
+    },
+    phone_email: {
+      required: true,
+    },
+    password: {
+      required: true,
+    },
+    birthdate: {
+      required: true,
+    },
+    radio: {
+      required: true,
+    },
   },
-  lastname: {
-    required: 'Campos inválidos',
+  messages: {
+    firstname: 'Campos inválidos',
+    lastname: 'Campos inválidos',
+    phone_email: 'Campos inválidos',
+    password: 'Campos inválidos',
+    birthdate: 'Campos inválidos',
+    radio: 'Campos inválidos',
   },
-  phone_email: {
-    required: 'Campos inválidos',
-  },
-  password: {
-    required: 'Campos inválidos',
-  },
-  birthdate: {
-    required: 'Campos inválidos',
-  },
-  gender: {
-    required: 'Campos inválidos',
-  },
-};
-
-const rulesValidate = {
-  rules: validateFields,
-  messages: warnings,
-};
+});
 
 window.onload = function () {
-  const formRules = new window.JustValidate('.form-rules', rulesValidate);
-  return formRules;
+  return validate;
 };

7602ada98bb84c5493677db2c1d6412a1eb05934
nyxnil <nsmnilson@hotmail.com>
2020-11-09 12:43:36 -0300

    chegando nos 90.6

--- script.js
+++ script.js
@@ -1,66 +1,56 @@
-const button = document.getElementById('button-login');
-const email = document.getElementById('user-email-phone');
-
-button.addEventListener('click', function () {
-  alert(email.value);
-});
-
-const feminino = document.getElementById('feminino');
-feminino.addEventListener('click', function () {
-  feminino.checked = true;
-});
-const masculino = document.getElementById('masculino');
-masculino.addEventListener('click', function () {
-  masculino.checked = true;
-});
 const divRadios = document.querySelector('.radios-container');
+const name = document.getElementById('name');
+const lastname = document.getElementById('lastname');
+const phoneEmail = document.getElementById('phoneEmail');
+const birthday = document.getElementById('date');
+const rightContent = document.querySelector('.right-content');
+const formName = document.querySelector('.name');
+const inputs = document.querySelectorAll('.input');
+
+function insertValuesParagraph(valueRadio) {
+  const p = document.createElement('p');
+  p.innerText = `Olá, ${name.value} ${lastname.value}`;
+  p.innerText += phoneEmail.value;
+  p.innerText += birthday.value;
+  p.innerText += valueRadio;
+  rightContent.innerHTML = '';
+  rightContent.appendChild(p);
+}
+
+function getValueRadios() {
+  const radios = document.querySelectorAll('.inputRadios');
+  for (let index = 0; index < radios.length; index += 1) {
+    if (radios[index].checked) {
+      return radios[index].value;
+    }
+  }
+  return false;
+}
+
+function checkEmptyFields() {
+  let result = true;
+  inputs.forEach((input) => {
+    if (input.value === '') result = false;
+  });
+  return result;
+}
+
+document.getElementById('button-login').addEventListener('click', function () {
+  alert(document.getElementById('user-email-phone').value);
+});
+
+document.getElementById('facebook-register').addEventListener('click', function (event) {
+  event.preventDefault();
+  const boolean = checkEmptyFields();
+  if (boolean) {
+    const valueRadio = getValueRadios();
+    insertValuesParagraph(valueRadio);
+  } else formName.innerHTML = 'Campos inválidos';
+});
+
 document.getElementById('personalizado').addEventListener('click', function () {
   const textInput = document.createElement('input');
   textInput.name = 'gender-custom';
   textInput.placeholder = 'Gênero (opcional)';
   divRadios.appendChild(textInput);
-});
-
-const facebookRegister = document.getElementById('facebook-register');
-facebookRegister.addEventListener('click', function (event) {
-  const personalizado = document.getElementById('personalizado');
-  event.preventDefault();
-  let camposVazios = 0;
-  const campos = document.querySelectorAll('.campo');
-  for (let index = 0; index < campos.length; index += 1) {
-    if (campos[index] === '') {
-      camposVazios += 1;
-    }
-  }
-  let gender = '';
-  if (feminino.checked !== false ||
-    masculino.checked !== false ||
-    personalizado.checked !== false) {
-    if (feminino.checked === true) {
-      gender = 'Feminino';
-    } else if (masculino.checked === true) {
-      gender = 'Masculino';
-    } else {
-      gender = 'Personalizado';
-    }
-  } else {
-    camposVazios += 1;
-  }
-
-  if (camposVazios > 0) {
-    const campoDeErro = document.createElement('p');
-    campoDeErro.innerHTML = 'Campos inválidos';
-    campoDeErro.style.color = 'red';
-    document.getElementById('formulario').appendChild(campoDeErro);
-  } else {
-    const firstname = document.getElementById('firstname');
-    const lastname = document.getElementById('lastname');
-    const phoneEmail = document.getElementById('phone_email');
-    const birthdate = document.getElementById('birthdate');
-    const rightContent = document.getElementById('inscricao');
-    rightContent.innerHTML = `Olá, ${firstname.value} ${lastname.value}
-        ${phoneEmail.value}
-        ${birthdate.value}
-        ${gender}`;
-  }
 });

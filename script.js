182ab562805b889f8c909a779aba0d773585ad8e
LukeCruz <enio.cruz@yahoo.com.br>
2020-11-10 16:25:03 -0300

    commit2

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 const buttonLogin = document.getElementById('button-login');
 const userEmailPhone = document.getElementById('user-email-phone');
+
 buttonLogin.addEventListener('click', function () {
   const text = userEmailPhone.value;
   alert(text);
 });
+
+let selectedGender = '';
+const genderContainer = document.getElementById('gender-container');
+genderContainer.addEventListener('click', function (event) {
+  selectedGender = event.target.value;
+});
+
 const buttonRegister = document.getElementById('facebook-register');
 buttonRegister.addEventListener('click', function (event) {
   event.preventDefault();
-  const fields = document.querySelectorAll('#form2 input');
+  const firstName = document.getElementById('firstname');
+  const lastName = document.getElementById('lastname');
+  const email = document.getElementById('phone_email');
+  const birthDate = document.getElementById('birthdate');
+  const rightContent = document.getElementById('right-content');
+  const fields = document.getElementsByTagName('input');
+  const form = document.getElementById('form2');
   for (let index = 0; index < fields.length; index += 1) {
     if (fields[index].value === '') {
-      const form = document.getElementById('form2');
-      form.innerHTML = 'Campos inválidos';
+        form.innerText = 'Campos inválidos';
+        // break;
+      } else {
+        rightContent.innerHTML = `Olá, ${firstName.value} ${lastName.value}.
+        Seu email é ${email.value}.
+        Sua data de nascimento é ${birthDate.value}.
+        Seu gênero é ${selectedGender}`;
+      }
     }
-}
-});
-
-const other = document.getElementById('Personalizado');
-other.addEventListener('click', function () {
-  const newField = document.createElement('input');
-  newField.type = 'text';
-  newField.name = 'gender-custom';
-  newField.placeholder = 'Gênero (opcional)';
-  const genderContainer = document.getElementById('gender-container');
-  genderContainer.appendChild(newField);
-});
+  });
+  
+  const other = document.getElementById('Personalizado');
+  other.addEventListener('click', function () {
+    const newField = document.createElement('input');
+    newField.type = 'text';
+    newField.name = 'gender-custom';
+    newField.placeholder = 'Gênero (opcional)';
+    genderContainer.appendChild(newField);
+  });
